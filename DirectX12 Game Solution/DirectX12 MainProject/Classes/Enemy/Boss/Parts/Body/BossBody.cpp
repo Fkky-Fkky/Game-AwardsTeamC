57f6095f3e40ce44fca9d264ba01fa651507a02a
MSI\jfuki <jfukiage46@gmail.com>
2022-05-19 14:27:42 +0900

    [Update] ボス本体の素材更新

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
@@ -1,7 +1,7 @@
 #include "Classes/Enemy/Boss/Parts/Body/BossBody.h"
 
 void BossBody::Initialize() {
-	BossParts::Initialize(SimpleMath::Vector3(0.0f, 1.0f, 3.0f), SimpleMath::Vector3(0.0f, 0.0f, 0.0f));
+	BossParts::Initialize(SimpleMath::Vector3(0.0f, 0.0f, 8.0f), SimpleMath::Vector3(0.0f, 0.0f, 0.0f));
 }
 void BossBody::LoadAssets() {
 	BossParts::LoadAssets(L"Boss/boss.X");
