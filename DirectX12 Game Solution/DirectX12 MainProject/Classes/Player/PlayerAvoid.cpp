3e239e856a635b959204f944f9853ff483dce8f4
MSI\jfuki <jfukiage46@gmail.com>
2022-05-19 13:57:53 +0900

    [Update] プレイヤーの回避完成 回避にクールタイムをいれた クールタイム中でも移動できるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAvoid.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAvoid.cpp
@@ -2,43 +2,84 @@
 #include "Classes/Player/Player.h"
 
 PlayerAvoid::PlayerAvoid() {
+	action_state_ = READY;
+	
 	avoid_speed_   = 0.0f;
 	player_dest_x_ = 0.0f;
+	cool_time_  = 0.0f;
+	time_delta_ = 0.0f;
 
-	avoid_flag_		 = false;
 	invincible_flag_ = false;
 
 	player_rote_ = SimpleMath::Vector3::Zero;
 }
 
 void PlayerAvoid::Update(const float deltaTime, Player& player) {
-	SimpleMath::Vector3 pos_ = player.GetPlayerPosition();
-	SimpleMath::Vector3 rot_ = player.GetPlayerRotation();
+	pos_ = player.GetPlayerPosition();
+	rot_ = player.GetPlayerRotation();
 
-	if (!avoid_flag_) {
-		avoid_flag_		 = true;
-		invincible_flag_ = true;
-		player_rote_   = rot_;
-		float add_pos_ = (IsPayerRightWard()) ? -ADD_POS_X_ : ADD_POS_X_;
-		player_dest_x_ = pos_.x + add_pos_;
-		player_dest_x_ = std::clamp(player_dest_x_, PLAYER_LIMIT_MIN_POS_X_, PLAYER_LIMIT_MAX_POS_X_);
-	}
+	time_delta_ = deltaTime;
 
-	if (avoid_flag_) {
-		avoid_speed_ = std::min(avoid_speed_ + ADD_AVOID_SPEED_ * deltaTime, AVOID_MAX_SPEED_);
-		pos_.x = (IsPayerRightWard()) ?
-			std::max(pos_.x - avoid_speed_ * deltaTime, player_dest_x_) :
-			std::min(pos_.x + avoid_speed_ * deltaTime, player_dest_x_);
-	}
+	switch (action_state_)
+	{
+	case READY:
+		Ready();
+		break;
 
-	if (pos_.x == player_dest_x_) {
-		player_dest_x_ = 0.0f;
-		avoid_speed_   = 0.0f;
-		avoid_flag_		 = false;
-		invincible_flag_ = false;
-		player.SwitchState(PLAYER_STATE::WAIT);
+	case AVOID:
+		Avoid();
+		break;
+
+	case COOL_TIME:
+		CoolTime();
+		break;
 	}
 
 	player.SetPlayerPosition(pos_);
 	player.SetPlayerRotation(rot_);
+	if (action_state_ == ACTION_END) {
+		action_state_ = READY;
+		player.SwitchState(PLAYER_STATE::WAIT);
+	}
+}
+
+void PlayerAvoid::Ready() {	//回避に必要な変数の準備
+	invincible_flag_ = true;
+	player_rote_   = rot_;
+	float add_pos_ = (IsPayerRightWard()) ? -ADD_POS_X_ : ADD_POS_X_;
+	player_dest_x_ = pos_.x + add_pos_;
+	player_dest_x_ = std::clamp(player_dest_x_, PLAYER_LIMIT_MIN_POS_X_, PLAYER_LIMIT_MAX_POS_X_);
+	action_state_  = AVOID;
+}
+
+void PlayerAvoid::Avoid() {	//回避
+	avoid_speed_ = std::min(avoid_speed_ + ADD_AVOID_SPEED_ * time_delta_, AVOID_MAX_SPEED_);
+	pos_.x = (IsPayerRightWard()) ?
+		std::max(pos_.x - avoid_speed_ * time_delta_, player_dest_x_) :
+		std::min(pos_.x + avoid_speed_ * time_delta_, player_dest_x_);
+	if (pos_.x == player_dest_x_) {
+		invincible_flag_ = false;
+		avoid_speed_	 = 0.0f;
+		action_state_	 = COOL_TIME;
+	}
+}
+
+void PlayerAvoid::CoolTime() {	//クールタイム
+	cool_time_ += time_delta_;
+
+	//クールタイム中も移動可能
+	if (DXTK->KeyState->A) {
+		pos_.x -= PLAYER_MOVE_SPEED_ * time_delta_;
+		rot_.y  = PLAYER_ROTATION_ANGLE_;
+	}
+	if (DXTK->KeyState->D) {
+		pos_.x += PLAYER_MOVE_SPEED_ * time_delta_;
+		rot_.y  = -PLAYER_ROTATION_ANGLE_;
+	}
+
+	if (cool_time_ >= COOL_TIME_MAX) {
+		cool_time_	   = 0.0f;
+		player_dest_x_ = 0.0f;
+		action_state_  = ACTION_END;
+	}
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAvoid.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAvoid.h
@@ -9,19 +9,36 @@ public:
 	virtual void Update(const float deltaTime, Player& player);
 	bool IsInvincible() { return invincible_flag_; }
 private:
+	void Ready();
+	void Avoid();
+	void CoolTime();
+
 	bool IsPayerRightWard() { return player_rote_.y >= PLAYER_ROTATION_ANGLE_; }
+
+	int action_state_;
+
+	enum ACTION_STATE_ {
+		READY,
+		AVOID,
+		COOL_TIME,
+		ACTION_END
+	};
 
 	float avoid_speed_;
 	float player_dest_x_;
-	bool avoid_flag_;
+	float cool_time_;
+	float time_delta_;
 	bool invincible_flag_;
 
 	SimpleMath::Vector3 player_rote_;
+	SimpleMath::Vector3 pos_;
+	SimpleMath::Vector3 rot_;
 
 	const float PLAYER_ROTATION_ANGLE_  = 90.0f;
 	const float PLAYER_LIMIT_MAX_POS_X_ = 17.0f;
 	const float PLAYER_LIMIT_MIN_POS_X_ = -14.0f;
-	const float ADD_POS_X_ = 8.0f;
+	const float ADD_POS_X_ = 5.5f;
 	const float ADD_AVOID_SPEED_ = 100.0f;
 	const float AVOID_MAX_SPEED_ = 100.0f;
+	const float COOL_TIME_MAX = 0.25f;
 };
