3dcd791f47a06a094d2400419879d0274902ba2b
MSI\jfuki <jfukiage46@gmail.com>
2022-05-23 14:49:32 +0900

    [Update] RightBeatクラス整理

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightBeat.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightBeat.cpp
@@ -5,77 +5,68 @@ void RightBeat::Update(const float deltaTime, SimpleMath::Vector3 player_pos, Bo
 	pos_  = boss_handR_->GetHandPos();
 	rote_ = boss_handR_->GetRotation();
 
-	time_delta = deltaTime;
+	time_delta_ = deltaTime;
 
-	switch (boss_action_state)
+	switch (boss_action_state_)
 	{
-	case MOVE:
-		HandMove(player_pos);
-		break;
-
-	case ATTACK:
-		RightBeatAttack(boss);
-		break;
-
-	case RETURN_POSITION:
-		HandReturn();
-		break;
-
+	case READY:		Ready(player_pos);		break;
+	case ATTACK:	RightBeatAttack(boss);	break;
+	case RETURN:	HandReturn();			break;
 	case ACTION_END:
-		boss_action_state = MOVE;
+		boss_action_state_ = READY;
 		boss->ActionEnd();
 		break;
 	}
 
 	boss_handR_->SetHandPos(pos_);
 	boss_handR_->SetHandRote(rote_);
-
 }
 
-void RightBeat::HandMove(SimpleMath::Vector3 player_pos) {
+void RightBeat::Ready(SimpleMath::Vector3 player_pos) {	//çUåÇÇ…ïKópÇ»ïœêîÇÃê›íË
 	if (!player_pos_get_flag_) {
-		move_pos = player_pos;
+		move_dest_ = player_pos;
 		player_pos_get_flag_ = true;
 	}
 
-	pos_.z = std::max(pos_.z - 10.0f * time_delta, 0.0f);
+	pos_.z = std::max(pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
 
-	if (pos_.x < move_pos.x)
-		pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta, move_pos.x);
+	if (pos_.x < move_dest_.x)
+		pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, move_dest_.x);
 	else
-		pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta, move_pos.x);
+		pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, move_dest_.x);
 
-	if (pos_.x == move_pos.x)
-		boss_action_state = ATTACK;
+	if (pos_.x == move_dest_.x)
+		boss_action_state_ = ATTACK;
 }
 
-void RightBeat::RightBeatAttack(Boss* boss) {
+void RightBeat::RightBeatAttack(Boss* boss) {	//í@Ç´Ç¬ÇØçUåÇ
 	boss_handR_->SetAttackFlag(true);
-	beat_time_ += time_delta;
-	pos_.y += BEAT_SPEED_ * beat_time_ - HALF_ * BEAT_GRAVITY_ * beat_time_ * beat_time_;
-	rote_.x = std::max(rote_.x - 1.0f * time_delta, -XM_1DIV2PI);
+	beat_time_ += time_delta_;
+	float beat_ = BEAT_SPEED_ * beat_time_ - HALF_ * BEAT_GRAVITY_ * beat_time_ * beat_time_;
+	pos_.y += beat_;
+	rote_.x = std::max(rote_.x - ROTE_SPEED_ * time_delta_, -XM_1DIV2PI);
 
 	if (pos_.y <= 0.0f) {
 		pos_.y = 0.0f;
 		boss->PlayBeatSE();
 		DX12Effect.PlayOneShot("shock", pos_);
 		boss_handR_->SetAttackFlag(false);
-		boss_action_state = RETURN_POSITION;
-	}	
+		boss_action_state_ = RETURN;
+	}
 }
 
-void RightBeat::HandReturn() {
-	wait_time_ += time_delta;
+void RightBeat::HandReturn() {	//éËÇèâä˙à íuÇ÷à⁄ìÆ
+	wait_time_ += time_delta_;
 
-	if (wait_time_ >= 0.2f) {
+	if (wait_time_ >= WAIT_TIME_MAX_) {
 		if (pos_.x < HAND_R_INITIAL_POS_X_)
-			pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta, HAND_R_INITIAL_POS_X_);
+			pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, HAND_R_INITIAL_POS_X_);
 		else
-			pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta, HAND_R_INITIAL_POS_X_);
+			pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, HAND_R_INITIAL_POS_X_);
 
-		pos_.y = std::min(pos_.y + MOVE_SPEED_Y_ * time_delta, HAND_INITIAL_POS_Y_);
-		pos_.z = std::min(pos_.z + 10.0f * time_delta, HAND_INITIAL_POS_Z_);
-		rote_.x = std::min(rote_.x + 1.0f * time_delta, XM_PIDIV4);
+		pos_.y = std::min(pos_.y + MOVE_SPEED_Y_ * time_delta_, HAND_INITIAL_POS_Y_);
+		pos_.z = std::min(pos_.z + MOVE_SPEED_Z_ * time_delta_, HAND_INITIAL_POS_Z_);
+		rote_.x = std::min(rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV4);
 	}
 
 	if (pos_.y >= HAND_INITIAL_POS_Y_ && pos_.x == HAND_R_INITIAL_POS_X_) {
@@ -83,6 +74,6 @@ void RightBeat::HandReturn() {
 		beat_time_ = 0.0f;
 
 		player_pos_get_flag_ = false;
-		boss_action_state = ACTION_END;
+		boss_action_state_ = ACTION_END;
 	}
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightBeat.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightBeat.h
@@ -9,36 +9,30 @@ using namespace DirectX;
 class RightBeat : public BossAttack {
 public:
 	RightBeat() {
-		time_delta = 0.0f;
+		time_delta_ = 0.0f;
 		beat_time_ = 0.0f;
 		wait_time_ = 0.0f;
 		player_pos_get_flag_ = false;
-		boss_action_state = 0;
+		boss_action_state_ = 0;
 	}
 	virtual void Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss);
 
 private:
-	void HandMove(SimpleMath::Vector3 player_pos);
+	void Ready(SimpleMath::Vector3 player_pos);
 	void RightBeatAttack(Boss* boss);
 	void HandReturn();
 
+	int boss_action_state_;
 
-	float time_delta;
+	float time_delta_;
 	float beat_time_;
 	float wait_time_;
 
 	bool player_pos_get_flag_;
+	
 	SimpleMath::Vector3 pos_;
 	SimpleMath::Vector3 rote_;
-	SimpleMath::Vector3 move_pos;
+	SimpleMath::Vector3 move_dest_;
 
-
-	int boss_action_state;
-	enum BOSS_ACTION_STATE {
-		MOVE,
-		ATTACK,
-		RETURN_POSITION,
-		ACTION_END
-	};
-
+	const float WAIT_TIME_MAX_ = 0.2f;
 };
