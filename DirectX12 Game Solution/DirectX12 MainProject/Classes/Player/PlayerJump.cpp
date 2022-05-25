d6ded7ca09de10c0cfbf30aa428bcb6f907a099d
MSI\jfuki <jfukiage46@gmail.com>
2022-05-25 17:06:27 +0900

    [Update] プレイヤージャンプを上昇中と下降中それぞれスピード調整できるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerJump.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerJump.cpp
@@ -2,41 +2,54 @@
 #include "Base/DX12Effekseer.h"
 #include "Classes/Player/Player.h"
 
-PlayerJump::PlayerJump() {
-    flag_ = false;
-    time_ = 0.0f;
-}
-
 void PlayerJump::Update(const float deltaTime, Player& player) {
     SimpleMath::Vector3 pos_ = player.GetPlayerPosition();
     SimpleMath::Vector3 rot_ = player.GetPlayerRotation();
 
-    time_ += JUMP_TIME_SPEED_ * deltaTime;
-
-    if (!flag_) {
-        flag_ = true;
-        time_ = 0.0f;
+    if (!jump_flag_) {
+        jump_flag_ = true;
+        player_up_flag_ = true;
+        jump_time_ = 0.0f;
+        cool_time_ = 0.0f;
         DX12Effect.PlayOneShot("jump", pos_);
     }
 
-    if (flag_) {
-        pos_.y = -HALF_ * GRAVITY_ * time_ * time_ + V0_ * time_ + GROUND_Y_;
+    if (jump_flag_) {   //プレイヤージャンプ処理(上昇)
+        if (player_up_flag_) {
+            jump_time_ += deltaTime* DOWN_SPEED_;
+            old_player_pos_ = pos_;
+            pos_.y = -HALF_ * GRAVITY_ * jump_time_ * jump_time_ + V0_ * jump_time_ + GROUND_Y_;
+            if (old_player_pos_.y >= pos_.y) {
+                player_up_flag_ = false;
+            }
+        }
+        else {  //プレイヤージャンプ処理(下降)
+            jump_time_ += deltaTime;
+            pos_.y = -HALF_ * GRAVITY_ * jump_time_ * jump_time_ + V0_ * jump_time_ + GROUND_Y_;
+        }
+
+        if (pos_.y <= GROUND_Y_) {
+            pos_.y  = GROUND_Y_;
+            cool_time_ += deltaTime;
+            landing_flag_ = true;
+            if (cool_time_ >= COOL_TIME_MAX_) {
+                jump_flag_ = false;
+                landing_flag_ = false;
+                player.SwitchState(PLAYER_STATE::WAIT);
+            }
+        }
     }
 
-    if (pos_.y < GROUND_Y_) {
-        pos_.y = GROUND_Y_;
-        flag_ = false;
-        player.SwitchState(PLAYER_STATE::WAIT);
-    }
+    if (!landing_flag_) {
+        if (DXTK->KeyState->D) {
+            pos_.x += PLAYER_MOVE_SPEED_ * deltaTime;
+            rot_.y = -PLAYER_ROTATION_ANGLE_;
+        }
 
-    if (DXTK->KeyState->D) {
-        pos_.x += PLAYER_MOVE_SPEED_ * deltaTime;
-        rot_.y = -PLAYER_ROTATION_ANGLE_;
-    }
-
-    if (DXTK->KeyState->A) {
-        pos_.x -= PLAYER_MOVE_SPEED_ * deltaTime;
-        rot_.y = PLAYER_ROTATION_ANGLE_;
+        if (DXTK->KeyState->A) {
+            pos_.x -= PLAYER_MOVE_SPEED_ * deltaTime;
+            rot_.y = PLAYER_ROTATION_ANGLE_;
+        }
     }
 
     pos_.x = std::clamp(pos_.x, PLAYER_LIMIT_MIN_POS_X_, PLAYER_LIMIT_MAX_POS_X_);
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerJump.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerJump.h
@@ -4,18 +4,32 @@
 
 class PlayerJump : public PlayerState {
 public:
-	PlayerJump();
+	PlayerJump() {
+		jump_time_ = 0.0f;
+		cool_time_ = 0.0f;
+		jump_flag_ = false;
+		player_up_flag_ = false;
+		landing_flag_ = false;
+		old_player_pos_ = SimpleMath::Vector3::Zero;
+	}
 	~PlayerJump() {};
 
 	virtual void Update(const float deltaTime, Player& player);
 
 private:
-	float time_;
-	bool  flag_;
+	float jump_time_;
+	float cool_time_;
 
-	const float GRAVITY_ = 0.045f;
-	const float V0_ = 0.8f;
-	const float GROUND_Y_ = 0.0f;
+	bool  jump_flag_;
+	bool  player_up_flag_;
+	bool landing_flag_;
+
+	SimpleMath::Vector3 old_player_pos_;
+
+	const float GRAVITY_ = 20.0f;
+	const float V0_ = 16.0f;
 	const float HALF_ = 0.5f;
-	const float JUMP_TIME_SPEED_ = 60.0f;
+	const float DOWN_SPEED_ = 2.0f;
+	const float GROUND_Y_ = 0.0f;
+	const float COOL_TIME_MAX_ = 0.2f;
 };
