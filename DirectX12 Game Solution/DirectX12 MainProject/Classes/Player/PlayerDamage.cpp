242e625181676b3529a57029761ec56b42e8462d
MSI\jfuki <jfukiage46@gmail.com>
2022-06-01 19:17:42 +0900

    [Update] プレイヤー被ダメージ時処理完成

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.cpp
@@ -1,6 +1,36 @@
 #include "Classes/Player/PlayerDamage.h"
 #include "Classes/Player/Player.h"
 
-void PlayerDamage::Update(const float deltaTime, Player& player) {
+void PlayerDamage::Initialize() {
+	dmg_time = 0.0f;
+	hit_flag_ = false;
+}
 
+void PlayerDamage::Update(const float deltaTime, Player& player) {
+	pos_ = player.GetPlayerPosition();
+	rot_ = player.GetPlayerRotation();
+
+	player.SetMotion(PLAYER_MOTION::KNOCK_BACK);
+
+	if (pos_.y >= 0.0f) {
+		pos_.y = std::max(pos_.y - 15.0f * deltaTime, 0.0f);
+	}
+
+	if (!hit_flag_) {
+		HitProcessing();
+	}
+
+	dmg_time = std::min(dmg_time + deltaTime, DMG_TIME_MAX);
+	if (dmg_time >= DMG_TIME_MAX) {
+		dmg_time = 0.0f;
+		player.SwitchState(PLAYER_STATE::WAIT);
+	}
+
+	player.SetPlayerPosition(pos_);
+	player.SetPlayerRotation(rot_);
+}
+
+void PlayerDamage::HitProcessing() {
+	player_hp_ -= 1.0f;
+	hit_flag_ = true;
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.h
@@ -4,8 +4,27 @@
 
 class PlayerDamage : public PlayerState {
 public:
+	PlayerDamage() {
+		player_hp_ = 30.0f;
+		dmg_time   = 0.0f;
+		hit_flag_  = false;
+	}
+	~PlayerDamage() {};
+
+	virtual void Initialize();
 	virtual void Update(const float deltaTime, Player& player);
 
-private:
+	float GetPlayerHP() { return player_hp_; }
 
+private:
+	void HitProcessing();
+
+	float player_hp_;
+	float dmg_time;
+	bool hit_flag_;
+
+	SimpleMath::Vector3 pos_;
+	SimpleMath::Vector3 rot_;
+
+	const float DMG_TIME_MAX = 0.5f;
 };
