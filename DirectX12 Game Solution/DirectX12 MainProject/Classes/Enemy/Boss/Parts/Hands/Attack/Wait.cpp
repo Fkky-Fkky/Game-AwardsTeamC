d5e92057de2c27810566f2ffcd63e4dbfcb929c7
MSI\jfuki <jfukiage46@gmail.com>
2022-06-20 19:07:46 +0900

    [Update] ボスのHPによって待機時間が変化するようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.cpp
@@ -1,8 +1,23 @@
 #include "Classes/Enemy/Boss/Parts/Hands/Attack/Wait.h"
 #include "Classes/Enemy/Boss/Boss.h"
 
-void Wait::Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss){
-	if (wait_time_ < max_wait_time) {
+void Wait::Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss) {
+	float boss_hp_ = boss->GetBossHP();
+
+	bool normal_time_ = boss_hp_ <= HP_NORMAL_MAX_ && boss_hp_ > HP_NORMAL_MIN_;
+	bool hard_time_   = boss_hp_ <= HP_NORMAL_MIN_ && boss_hp_ > HP_HARD_MIN_;
+
+	if (normal_time_) {
+		max_wait_time_ = MAX_WAIT_TIME_NORMAL_;
+	}
+	else if (hard_time_) {
+		max_wait_time_ = MAX_WAIT_TIME_HARD_;
+	}
+	else {
+		max_wait_time_ = MAX_WAIT_TIME_VERY_HARD_;
+	}
+
+	if (wait_time_ < max_wait_time_) {
 		wait_time_ += deltaTime;
 	}
 	else {
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.h
@@ -8,11 +8,17 @@ class Wait : public BossAttack {
 public:
 	Wait() {
 		wait_time_ = 0.0f;
-		max_wait_time = 0.1f;
+		max_wait_time_ = 0.1f;
 	}
 	virtual void Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss);
 
 private:
 	float wait_time_;
-	float max_wait_time;
+	float max_wait_time_;
+	const float MAX_WAIT_TIME_NORMAL_	 = 0.5f;
+	const float MAX_WAIT_TIME_HARD_		 = 0.3f; 
+	const float MAX_WAIT_TIME_VERY_HARD_ = 0.1f;
+	const float HP_NORMAL_MAX_ = 30.0f;
+	const float HP_NORMAL_MIN_ = 20.0f;
+	const float HP_HARD_MIN_ = 10.0f;
 };
