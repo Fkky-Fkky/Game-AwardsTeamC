2f1540c64d09430445c8ca5938fdba064503902c
MSI\jfuki <jfukiage46@gmail.com>
2022-06-20 19:09:05 +0900

    [Update] 右薙ぎ払い攻撃に予備動作追加

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightSlap.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightSlap.cpp
@@ -8,26 +8,43 @@ void RightSlap::Update(const float deltaTime, SimpleMath::Vector3 player_pos, Bo
 	time_delta_ = deltaTime;
 
 	switch (action_state_) {
+	case READY:			Ready();				break;
+	case WAIT:			Wait();					break;
 	case ATTACK:		RightSlapAttack(boss);	break;
 	case RESET:			Reset();				break;
 	case RETURN:		HandReturn();			break;
-	case ACTION_END:
-		boss->ActionEnd();
-		action_state_ = ATTACK;					break;
+	case ACTION_END:	boss->ActionEnd();		break;
 	}
 
 	boss_handR_->SetHandPos(pos_);
 	boss_handR_->SetHandRote(rote_);
 }
 
+void RightSlap::Ready() {	//予備動作
+	slap_time_y_ += time_delta_;
+	float slap_y_ = SLAP_SPEED_Y_ * slap_time_y_ - HALF_ * SLAP_GRAVITY_Y_ * slap_time_y_ * slap_time_y_;
+	pos_.y += slap_y_;
+	pos_.z  = std::max(pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
+	rote_.x = std::min(rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV2);
+
+	if (pos_.y <= SLAP_POS_Y_) {
+		pos_.y  = SLAP_POS_Y_;
+		action_state_ = WAIT;
+	}
+}
+
+void RightSlap::Wait() {	//待機
+	wait_time_ = std::min(wait_time_ + time_delta_, WAIT_TIME_MAX_);
+	if (wait_time_ >= WAIT_TIME_MAX_) {
+		action_state_ = ATTACK;
+	}
+}
+
 void RightSlap::RightSlapAttack(Boss* boss) {	//右手薙ぎ払い攻撃
 	boss_handR_->SetAttackFlag(true);
-	slap_time_ += time_delta_;
-	float slap_ = SLAP_SPEED_ * slap_time_ - HALF_ * SLAP_GRAVITY_ * slap_time_ * slap_time_;
-	pos_.x -= slap_;
-	pos_.y = std::max(pos_.y - MOVE_SPEED_Y_ * time_delta_, SLAP_POS_Y_);
-	pos_.z = std::max(pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
-	rote_.x = std::min(rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV2);
+	slap_time_x_ += time_delta_;
+	float slap_x_ = SLAP_SPEED_X_ * slap_time_x_ - HALF_ * SLAP_GRAVITY_X_ * slap_time_x_ * slap_time_x_;
+	pos_.x -= slap_x_;
 	if (!is_se_play_) {
 		boss->PlaySlapSE();
 		is_se_play_ = true;
@@ -43,14 +60,13 @@ void RightSlap::Reset() {	//手を画面の反対側に移動
 	pos_.y  = HAND_INITIAL_POS_Y_;
 	pos_.z  = HAND_INITIAL_POS_Z_;
 	rote_.x = XM_PIDIV4;
-	is_se_play_ = false;
+	is_se_play_	  = false;
 	action_state_ = RETURN;
 }
 
 void RightSlap::HandReturn() {	//画面外から初期位置へ移動
 	pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, HAND_R_INITIAL_POS_X_);
 	if (pos_.x >= HAND_R_INITIAL_POS_X_ ) {
-		slap_time_ = 0.0f;
 		action_state_ = ACTION_END;
 	}
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightSlap.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/RightSlap.h
@@ -5,24 +5,30 @@
 class RightSlap : public BossAttack {
 public:
 	RightSlap() {
-		action_state_ = ATTACK;
-		slap_time_	= 0.0f;
-		time_delta_ = 0.0f;
-		is_se_play_ = false;
+		action_state_ = READY;
+		time_delta_	  = 0.0f;
+		slap_time_x_  = 0.0f;
+		slap_time_y_  = 0.0f;
+		wait_time_	  = 0.0f;
+		is_se_play_   = false;
 		pos_  = SimpleMath::Vector3::Zero;
 		rote_ = SimpleMath::Vector3::Zero;
 	}
 	virtual void Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss);
 
 private:
+	void Ready();
+	void Wait();
 	void RightSlapAttack(Boss* boss);
 	void Reset();
 	void HandReturn();
 
 	int action_state_;
 
-	float slap_time_;
 	float time_delta_;
+	float slap_time_x_;
+	float slap_time_y_;
+	float wait_time_;
 
 	bool is_se_play_;
 
