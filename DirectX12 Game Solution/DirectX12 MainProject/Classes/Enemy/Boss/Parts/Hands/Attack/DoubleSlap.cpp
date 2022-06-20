d79369431245ef6c793b41a0fde7e02961a5fbd1
MSI\jfuki <jfukiage46@gmail.com>
2022-06-20 19:08:25 +0900

    [Update] 同時薙ぎ払い攻撃に予備動作追加

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.cpp
@@ -10,12 +10,11 @@ void DoubleSlap::Update(const float deltaTime, SimpleMath::Vector3 player_pos, B
 	time_delta_ = deltaTime;
 
 	switch (action_state_) {
-	case ATTACK:	Attack();		break;
-	case RESET:		Reset();		break;
-	case RETURN:	HandReturn();	break;
-	case ACTION_END:
-		boss->ActionEnd();
-		action_state_ = ATTACK;		break;
+	case READY:			Ready();			break;
+	case ATTACK:		Attack();			break;
+	case RESET:			Reset();			break;
+	case RETURN:		HandReturn();		break;
+	case ACTION_END:	boss->ActionEnd();	break;
 	}
 
 	boss_handR_->SetHandPos(r_pos_);
@@ -24,11 +23,47 @@ void DoubleSlap::Update(const float deltaTime, SimpleMath::Vector3 player_pos, B
 	boss_handL_->SetHandRote(l_rote_);
 }
 
-void DoubleSlap::Attack() {
-	SlapR();
+void DoubleSlap::Ready() {	//予備動作
+	ReadyR();
+	ReadyL();
 
+	if (ready_flag_r_ && ready_flag_l_) {
+		action_state_ = ATTACK;
+	}
+}
+
+void DoubleSlap::ReadyR() {	//右手構え
+	r_slap_time_y_ += time_delta_;
+	float r_slap_y_ = SLAP_SPEED_Y_ * r_slap_time_y_ - HALF_ * SLAP_GRAVITY_Y_ * r_slap_time_y_ * r_slap_time_y_;
+	r_pos_.y += r_slap_y_;
+	r_pos_.z  = std::max(r_pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
+	r_rote_.x = std::min(r_rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV2);
+	if (r_pos_.y <= R_HAND_DEST_Y_) {
+		r_pos_.y  = R_HAND_DEST_Y_;
+		ready_flag_r_ = true;
+	}
+}
+
+void DoubleSlap::ReadyL() {	//左手構え
+	l_slap_time_y_ += time_delta_;
+	float l_slap_y_ = SLAP_SPEED_Y_ * l_slap_time_y_ - HALF_ * SLAP_GRAVITY_Y_ * l_slap_time_y_ * l_slap_time_y_;
+	l_pos_.y += l_slap_y_;
+	l_pos_.z  = std::max(l_pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
+	l_rote_.x = std::min(l_rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV2);
+	if (l_pos_.y <= L_HAND_DEST_Y_) {
+		l_pos_.y  = L_HAND_DEST_Y_;
+		ready_flag_l_ = true;
+	}
+}
+
+void DoubleSlap::Attack() {	//薙ぎ払い攻撃
 	wait_time_ = std::min(wait_time_ + time_delta_, WAIT_TIME_MAX_);
-	if (wait_time_ == WAIT_TIME_MAX_) {
+	
+	if (wait_time_ >= ATTACK_START_TIME_R_) {
+		SlapR();
+	}
+	
+	if (wait_time_ >= WAIT_TIME_MAX_) {
 		SlapL();
 	}
 
@@ -38,27 +73,21 @@ void DoubleSlap::Attack() {
 	}
 }
 
-void DoubleSlap::SlapR() {
+void DoubleSlap::SlapR() {	//右手薙ぎ払い
 	boss_handR_->SetAttackFlag(true);
-	r_slap_time_ += time_delta_;
-	float r_slap_ = SLAP_SPEED_ * r_slap_time_ - HALF_ * SLAP_GRAVITY_ * r_slap_time_ * r_slap_time_;
-	r_pos_.x -= r_slap_;
-	r_pos_.y  = std::max(r_pos_.y  - MOVE_SPEED_Y_ * time_delta_, R_HAND_DEST_Y_);
-	r_pos_.z  = std::max(r_pos_.z  - MOVE_SPEED_Z_ * time_delta_, 0.0f);
-	r_rote_.x = std::min(r_rote_.x + ROTE_SPEED_   * time_delta_, XM_PIDIV2);
+	r_slap_time_x_  += time_delta_;
+	float r_slap_x_ = SLAP_SPEED_X_ * r_slap_time_x_ - HALF_ * SLAP_GRAVITY_X_ * r_slap_time_x_ * r_slap_time_x_;
+	r_pos_.x -= r_slap_x_;
 }
 
-void DoubleSlap::SlapL() {
+void DoubleSlap::SlapL() {	//左手薙ぎ払い
 	boss_handL_->SetAttackFlag(true);
-	l_slap_time_ += time_delta_;
-	float l_slap_ = SLAP_SPEED_ * l_slap_time_ - HALF_ * SLAP_GRAVITY_ * l_slap_time_ * l_slap_time_;
-	l_pos_.x += l_slap_;
-	l_pos_.y  = std::max(l_pos_.y  - MOVE_SPEED_Y_ * time_delta_, L_HAND_DEST_Y_);
-	l_pos_.z  = std::max(l_pos_.z  - MOVE_SPEED_Z_ * time_delta_, 0.0f);
-	l_rote_.x = std::min(l_rote_.x + ROTE_SPEED_   * time_delta_, XM_PIDIV2);
+	l_slap_time_x_ += time_delta_;
+	float l_slap_x_ = SLAP_SPEED_X_ * l_slap_time_x_ - HALF_ * SLAP_GRAVITY_X_ * l_slap_time_x_ * l_slap_time_x_;
+	l_pos_.x += l_slap_x_;
 }
 
-void DoubleSlap::Reset() {
+void DoubleSlap::Reset() {	//それぞれの手を画面の反対へ移動
 	r_pos_.x  = -HAND_RETURN_POS_X_;
 	r_pos_.y  = HAND_INITIAL_POS_Y_;
 	r_pos_.z  = HAND_INITIAL_POS_Z_;
@@ -71,12 +100,10 @@ void DoubleSlap::Reset() {
 	l_rote_.x = XM_PIDIV4;
 	boss_handL_->SetAttackFlag(false);
 
-	wait_time_ = 0.0f;
-
 	action_state_ = RETURN;
 }
 
-void DoubleSlap::HandReturn() {
+void DoubleSlap::HandReturn() {	//手を元の座標に戻す
 	r_pos_.x = std::min(r_pos_.x + MOVE_SPEED_X_ * time_delta_, HAND_R_INITIAL_POS_X_);
 	l_pos_.x = std::max(l_pos_.x - MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
 
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.h
@@ -10,18 +10,26 @@ public:
 		l_pos_  = SimpleMath::Vector3::Zero;
 		l_rote_ = SimpleMath::Vector3::Zero;
 
-		action_state_ = ATTACK;
+		action_state_ = READY;
 
-		time_delta_  = 0.0f;
-		r_slap_time_ = 0.0f;
-		l_slap_time_ = 0.0f;
-		wait_time_	 = 0.0f;
+		time_delta_	   = 0.0f;
+		r_slap_time_x_ = 0.0f;
+		r_slap_time_y_ = 0.0f;
+		l_slap_time_x_ = 0.0f;
+		l_slap_time_y_ = 0.0f;
+		wait_time_	   = 0.0f;
+
+		ready_flag_r_ = false;
+		ready_flag_l_ = false;
 	}
 
 	~DoubleSlap() {};
 	virtual void Update(const float deltaTime, SimpleMath::Vector3 player_pos, Boss* boss);
 
 private:
+	void Ready();
+	void ReadyR();
+	void ReadyL();
 	void Attack();
 	void SlapR();
 	void SlapL();
@@ -36,11 +44,18 @@ private:
 	int action_state_;
 
 	float time_delta_;
-	float r_slap_time_;
-	float l_slap_time_;
+	float r_slap_time_x_;
+	float r_slap_time_y_;
+	float l_slap_time_x_;
+	float l_slap_time_y_;
+
 	float wait_time_;
+
+	bool ready_flag_r_;
+	bool ready_flag_l_;
 
 	const float R_HAND_DEST_Y_  = 2.0f;
 	const float L_HAND_DEST_Y_  = 8.0f;
-	const float WAIT_TIME_MAX_  = 1.0f;
+	const float ATTACK_START_TIME_R_ = 0.5f;
+	const float WAIT_TIME_MAX_		 = 1.0f;
 };
