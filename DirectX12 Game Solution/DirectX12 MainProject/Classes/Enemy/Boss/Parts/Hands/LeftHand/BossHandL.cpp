8a2b6069a6e60f23e6c48d5dbf08ce5b1383a4a6
MSI\jfuki <jfukiage46@gmail.com>
2022-06-20 19:04:12 +0900

    [Update] 左手をモーション付きモデルに対応した

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.cpp
@@ -1,12 +1,50 @@
 #include "Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.h"
 
 void BossHandL::Initialize() {
-	BossHand::Initialize(
+	BossParts::Initialize(
 		SimpleMath::Vector3(INITIAL_POS_X, INITIAL_POS_Y, INITIAL_POS_Z),
 		SimpleMath::Vector3(XM_PIDIV4, -5.0f, 0.0f)
 	);
 }
 
 void BossHandL::LoadAssets() {
-	BossHand::LoadAssets(L"Boss/boss_hand_L.X");
+	hand_l_ = DX9::SkinnedModel::CreateFromFile(DXTK->Device9, L"Boss/boss_hand_L.X");
+	
+	collision = hand_l_->GetBoundingOrientedBox();
+	collision.Extents = SimpleMath::Vector3(
+		collision.Extents.x * 0.5f,
+		collision.Extents.y * 0.3f,
+		collision.Extents.z * 0.3f
+	);
+
+	collision_model = DX9::Model::CreateBox(
+		DXTK->Device9,
+		collision.Extents.x,
+		collision.Extents.y,
+		collision.Extents.z
+	);
+	D3DMATERIAL9 material{};
+	material.Diffuse = DX9::Colors::Value(0.0f, 1.0f, 0.0f, 0.75f);
+	collision_model->SetMaterial(material);
+
+	for (int i = 0; i < 2; ++i) {
+		hand_l_->SetTrackEnable(i, false);
+	}
+	hand_l_->SetTrackEnable(1, true);
+}
+
+void BossHandL::Update(const float deltaTime) {
+	hand_l_->AdvanceTime(deltaTime);
+	collision.Center = hand_l_->GetPosition();
+	collision.Orientation = hand_l_->GetRotationQuaternion();
+}
+
+void BossHandL::Render() {
+	hand_l_->SetPosition(position);
+	hand_l_->SetRotation(rotation);
+	hand_l_->Draw();
+
+	collision_model->SetPosition(collision.Center);
+	collision_model->SetRotationQuaternion(collision.Orientation);
+	collision_model->Draw();
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.h
@@ -6,7 +6,9 @@ class BossHandL : public BossHand {
 public:
 	void Initialize();
 	void LoadAssets();
-
+	void Update(const float deltaTime);
+	void Render();
 private:
+	DX9::SKINNEDMODEL hand_l_;
 	const float INITIAL_POS_X = 13.0f;
 };
