8e064c428394ff7284f30abcce1714dee5af7015
MSI\jfuki <jfukiage46@gmail.com>
2022-06-20 19:08:48 +0900

    [Update] 左薙ぎ払い攻撃に予備動作追加

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftSlap.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftSlap.cpp
@@ -8,26 +8,43 @@ void LeftSlap::Update(const float deltaTime, SimpleMath::Vector3 player_pos, Bos
 	time_delta_ = deltaTime;
 
 	switch (action_state_) {
-	case ATTACK:	LeftSlapAttack(boss);	break;
-	case RESET:		Reset();				break;
-	case RETURN:	HandReturn();			break;
-	case ACTION_END:
-		boss->ActionEnd();
-		action_state_ = ATTACK;				break;
+	case READY:			Ready();				break;
+	case WAIT:			Wait();					break;
+	case ATTACK:		LeftSlapAttack(boss);	break;
+	case RESET:			Reset();				break;
+	case RETURN:		HandReturn();			break;
+	case ACTION_END:	boss->ActionEnd();		break;
 	}
 
 	boss_handL_->SetHandPos(pos_);
 	boss_handL_->SetHandRote(rote_);
 }
 
-void LeftSlap::LeftSlapAttack(Boss* boss) {	//¶è“ã‚¬•¥‚¢UŒ‚
-	boss_handL_->SetAttackFlag(true);
-	slap_time_ += time_delta_;
-	float slap_ = SLAP_SPEED_ * slap_time_ - HALF_ * SLAP_GRAVITY_ * slap_time_ * slap_time_;
-	pos_.x += slap_;
-	pos_.y  = std::max(pos_.y - MOVE_SPEED_Y_ * time_delta_, SLAP_POS_Y_);
+void LeftSlap::Ready() {	//—\”õ“®ì
+	slap_time_y_ += time_delta_;
+	pos_.y += SLAP_SPEED_Y_ * slap_time_y_ - HALF_ * SLAP_GRAVITY_Y_ * slap_time_y_ * slap_time_y_;
 	pos_.z  = std::max(pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
 	rote_.x = std::min(rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV2);
+
+	if (pos_.y <= SLAP_POS_Y_) {
+		pos_.y  = SLAP_POS_Y_;
+		action_state_ = WAIT;
+	}
+}
+
+void LeftSlap::Wait() {	//‘Ò‹@
+	wait_time_ = std::min(wait_time_ + time_delta_, WAIT_TIME_MAX_);
+	if (wait_time_ >= WAIT_TIME_MAX_) {
+		action_state_ = ATTACK;
+	}
+}
+
+void LeftSlap::LeftSlapAttack(Boss* boss) {	//¶è“ã‚¬•¥‚¢UŒ‚
+	boss_handL_->SetAttackFlag(true);
+	slap_time_x_ += time_delta_;
+	float slap_x_ = SLAP_SPEED_X_ * slap_time_x_ - HALF_ * SLAP_GRAVITY_X_ * slap_time_x_ * slap_time_x_;
+	pos_.x += slap_x_;
+	
 	if (!is_se_play_) {
 		boss->PlaySlapSE();
 		is_se_play_ = true;
@@ -50,7 +67,6 @@ void LeftSlap::Reset() {	//è‚ğ‰æ–Ê‚Ì”½‘Î‘¤‚ÉˆÚ“®
 void LeftSlap::HandReturn() {	//‰æ–ÊŠO‚©‚ç‰ŠúˆÊ’u‚ÖˆÚ“®
 	pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
 	if (pos_.x == HAND_L_INITIAL_POS_X_) {
-		slap_time_	  = 0.0f;
 		action_state_ = ACTION_END;
 	}
 }
