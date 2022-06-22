05a8f7bf6b62f909bf8fb3ea448f6cff1ebb5d8f
MSI\jfuki <jfukiage46@gmail.com>
2022-06-22 11:33:58 +0900

    [Fix] 手被ダメージ時HPが減らないバグ解消 デバックテキスト座標調整

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Collision/Collision.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Collision/Collision.cpp
@@ -2,9 +2,10 @@
 #include "Classes/Collision/ObjectManager.h"
 
 void Collision::Initialize() {
+	hand_dmg_flag_reset_time_ = 0.0f;
 	player_dmg_flag_r_ = false;
 	player_dmg_flag_l_ = false;
-	boss_dmg_flg_ = false;
+	boss_core_dmg_flg_	  = false;
 	boss_hand_r_dmg_flag_ = false;
 	boss_hand_l_dmg_flag_ = false;
 	is_boss_hand_open_	  = false;
@@ -27,7 +28,7 @@ void Collision::Update(const float deltaTime, ObjectManager* obj_m_) {
 
 	is_boss_hand_open_ = obj_m_->IsBossHandOpen();
 
-	if (!player_invincible_flag) {
+	if (!player_invincible_flag) {	//プレイヤー:ボス攻撃の判定
 		if (boss_r_atk_flag_) {
 			player_dmg_flag_r_ = player_col_.Intersects(boss_r_hand_col_);
 		}
@@ -43,15 +44,22 @@ void Collision::Update(const float deltaTime, ObjectManager* obj_m_) {
 		}
 	}
 	
-	if (player_atk_flag_) {
-		boss_dmg_flg_ = player_atk_col_.Intersects(boss_core_col_);
-		if (is_boss_hand_open_) {
-			boss_hand_r_dmg_flag_ = player_atk_col_.Intersects(boss_r_hand_col_);
-			boss_hand_l_dmg_flag_ = player_atk_col_.Intersects(boss_l_hand_col_);
-		}
+	if (player_atk_flag_) {	//プレイヤー攻撃:ボスコアの判定
+		boss_core_dmg_flg_ = player_atk_col_.Intersects(boss_core_col_);
 	}
 	else {
-		boss_dmg_flg_ = false;
+		boss_core_dmg_flg_ = false;
+	}
+
+	if (is_boss_hand_open_) {	//プレイヤー攻撃:ボス手の判定
+		if (player_atk_flag_) {
+			boss_hand_r_dmg_flag_ = player_atk_col_.Intersects(boss_r_hand_col_);
+			boss_hand_l_dmg_flag_ = player_atk_col_.Intersects(boss_l_hand_col_);
+			hand_dmg_flag_reset_time_ = 1.0f;
+		}
+	}
+	hand_dmg_flag_reset_time_ = std::max(hand_dmg_flag_reset_time_ - deltaTime, 0.0f);
+	if (hand_dmg_flag_reset_time_ <= 0.0f) {	//一定時間経過でダメージフラグ降ろす
 		boss_hand_r_dmg_flag_ = false;
 		boss_hand_l_dmg_flag_ = false;
 	}
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Collision/Collision.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Collision/Collision.h
@@ -10,33 +10,35 @@ class ObjectManager;
 class Collision {
 public:
 	Collision() {
+		hand_dmg_flag_reset_time_ = 0.0f;
 		player_dmg_flag_r_ = false;
 		player_dmg_flag_l_ = false;
-		boss_dmg_flg_	   = false;
+		boss_core_dmg_flg_	  = false;
 		boss_hand_r_dmg_flag_ = false;
 		boss_hand_l_dmg_flag_ = false;
 		is_boss_hand_open_	  = false;
 	}
-
 	~Collision() {};
 
 	void Initialize();
 	void LoadAssets();
 	void Update(const float deltaTime, ObjectManager* obj_m_);
 	void Render2D();
+
 	bool GetPlayerDmgFlagR() { return player_dmg_flag_r_; }
 	bool GetPlayerDmgFlagL() { return player_dmg_flag_l_; }
-	bool GetBossDmgFlag() { return boss_dmg_flg_; }
+	bool GetBossDmgFlag() { return boss_core_dmg_flg_; }
 	bool IsBossHandRDmg() { return boss_hand_r_dmg_flag_; }
 	bool IsBossHandLDmg() { return boss_hand_l_dmg_flag_; }
 
 private:
+	DX9::SPRITEFONT font;
 
+	float hand_dmg_flag_reset_time_;
 	bool player_dmg_flag_r_; 
 	bool player_dmg_flag_l_;
-	bool boss_dmg_flg_;
+	bool boss_core_dmg_flg_;
 	bool boss_hand_r_dmg_flag_;
 	bool boss_hand_l_dmg_flag_;
 	bool is_boss_hand_open_;
-	DX9::SPRITEFONT font;
 };
