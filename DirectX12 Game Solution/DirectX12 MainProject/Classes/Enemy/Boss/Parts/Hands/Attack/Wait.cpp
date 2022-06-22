f7f0c62887d02e4e3438a20654f42cf00f5ac629
MSI\jfuki <jfukiage46@gmail.com>
2022-06-22 19:02:47 +0900

    [Update] コメントの追加 手のHPが両方0になったら待機時間を変えるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Wait.cpp
@@ -7,14 +7,19 @@ void Wait::Update(const float deltaTime, ObjectManager* obj_m, Boss* boss) {
 	bool normal_time_ = boss_hp_ <= HP_NORMAL_MAX_ && boss_hp_ > HP_NORMAL_MIN_;
 	bool hard_time_   = boss_hp_ <= HP_NORMAL_MIN_ && boss_hp_ > HP_HARD_MIN_;
 
-	if (normal_time_) {
+	if (normal_time_) {	//ボスHP3/3時の待機時間
 		max_wait_time_ = MAX_WAIT_TIME_NORMAL_;
 	}
-	else if (hard_time_) {
+	else if (hard_time_) {	//ボスHP2/3時の待機時間
 		max_wait_time_ = MAX_WAIT_TIME_HARD_;
 	}
-	else {
+	else {	//ボスHP1/3時の待機時間
 		max_wait_time_ = MAX_WAIT_TIME_VERY_HARD_;
+	}
+
+	if (boss_handL_->GetHandHp() <= 0 &&
+		boss_handR_->GetHandHp() <= 0) {
+		max_wait_time_ = 5.0f;
 	}
 
 	wait_time_ = std::min(wait_time_ + deltaTime, max_wait_time_);
