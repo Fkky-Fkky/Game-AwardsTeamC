0752271e4e3a5d07c3d36ea5990ebad3cb4f2693
MSI\jfuki <jfukiage46@gmail.com>
2022-06-23 17:55:09 +0900

    [Update] Groundクラスコード整理

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
@@ -3,15 +3,18 @@
 
 void Ground::LoadAssets() {
     stage_flooring_ = DX9::Model::CreateFromFile(DXTK->Device9, L"Ground/Stage/Flooring/flooring.X");
-    stage_bed_      = DX9::Model::CreateFromFile(DXTK->Device9, L"Ground/Stage/Bet/bet.X");
-    building_block  = DX9::SkinnedModel::CreateFromFile(DXTK->Device9, L"Ground/Stage/Flooring/object1.X");
+    stage_flooring_->SetPosition(SimpleMath::Vector3::Zero);
+
+    stage_bed_ = DX9::Model::CreateFromFile(DXTK->Device9, L"Ground/Stage/Bet/bet.X");
+    stage_bed_->SetPosition(SimpleMath::Vector3(0.0f, BED_POS_Y_, BED_POS_Z_));
+
+    building_block = DX9::SkinnedModel::CreateFromFile(DXTK->Device9, L"Ground/Stage/Flooring/object1.X");
+    building_block->SetPosition(SimpleMath::Vector3::Zero);
     building_block->SetTrackEnable(0, true);
 
-    //crack_[3] = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BackGround/hibiware1.png");
-    //crack_[2] = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BackGround/hibiware2.png");
-    //crack_[1] = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BackGround/hibiware3.png");
-    //crack_[0] = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BackGround/hibiware4.png");
-    
+    castle = DX9::Model::CreateFromFile(DXTK->Device9, L"Ground/Stage/Bet/castle.X");
+    //castle->SetScale(0.7f);
+
     bgm_main_ = DX9::MediaRenderer::CreateFromFile(DXTK->Device9, L"BGM/game_main.mp3");
     bgm_main_->Play();
 }
@@ -19,29 +22,20 @@ void Ground::LoadAssets() {
 void Ground::Update(const float deltaTime, ObjectManager* object) {
     building_block->AdvanceTime(deltaTime);
 
-    if (bgm_main_->isComplete())
+    if (bgm_main_->isComplete()) {
         bgm_main_->Replay();
-
-    int boss_hp_ = object->GetBossHP();
-
-    const int TEN = 10;
-    crack_level_ = boss_hp_ / TEN;
+    }
 }
 
 void Ground::Render() {
-    stage_bed_->SetPosition(SimpleMath::Vector3(0.0f, BED_POS_Y_, BED_POS_Z_));
     stage_bed_->Draw();
-
-    stage_flooring_->SetPosition(SimpleMath::Vector3::Zero);
     stage_flooring_->Draw();
-
-    building_block->SetPosition(SimpleMath::Vector3::Zero);
     building_block->Draw();
+
+    //castle->SetPosition(SimpleMath::Vector3(0.0f, CASTLE_POS_Y_, BED_POS_Z_));
+    //castle->Draw();
 }
 
 void Ground::Render2D() {
-    //DX9::SpriteBatch->DrawSimple(
-    //    crack_[crack_level_].Get(),
-    //    SimpleMath::Vector3(0.0f, 0.0f, CRACK_POS_Z_)
-    //);
+
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
@@ -9,9 +9,7 @@ using namespace DirectX;
 
 class Ground {
 public:
-	Ground() {
-		crack_level_ = 0;
-	}
+	Ground() {};
 	~Ground() {};
 
 	void LoadAssets();
@@ -22,13 +20,11 @@ public:
 private:
 	DX9::MODEL stage_flooring_;
 	DX9::MODEL stage_bed_;
+	DX9::MODEL castle;
 	DX9::SKINNEDMODEL building_block;
-	//DX9::SPRITE crack_[4];
 	DX9::MEDIARENDERER bgm_main_;
-
-	int crack_level_;
 
 	const float BED_POS_Y_ = -4.5f;
 	const float BED_POS_Z_ = 12.0f;
-	const float CRACK_POS_Z_  = 10000.0f;
+	const float CASTLE_POS_Y_ = -6.19f;
 };
