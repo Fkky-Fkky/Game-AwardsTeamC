015e3a4ebaa0bd6d3ddb6b09cc2cd0a755335fc1
MSI\jfuki <jfukiage46@gmail.com>
2022-06-23 16:22:25 +0900

    [Update] コアの座標調整 当たり判定調整 Weak状態になった時にコアがステージに出てくるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Core/BossCore.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Core/BossCore.cpp
@@ -1,9 +1,13 @@
 #include "Classes/Enemy/Boss/Parts/Core/BossCore.h"
+#include "Classes/Enemy/Boss/Boss.h"
 
 void BossCore::Initialize() {
-	core_pos_.y = BOSS_CORE_POS_Y_;
+	core_pos_.y = CORE_INTIAL_POS_Y_;
+	core_pos_.z = CORE_INTIAL_POS_Z_;
 	BossParts::Initialize(core_pos_, SimpleMath::Vector3::Zero);
 	core_hp_  = BOSS_MAX_HP_;
+	time_delta_ = 0.0f;
+	core_weak_time_ = 0.0f;
 	hit_flag_ = false;
 }
 
@@ -11,17 +15,41 @@ void BossCore::LoadAssets() {
 	BossParts::LoadAssets(L"Boss/boss_Core.X");
 	
 	collision_ = model->GetBoundingOrientedBox();
-	collision_.Extents = SimpleMath::Vector3(collision_.Extents);
+	collision_.Extents = SimpleMath::Vector3(
+		collision_.Extents.x * 0.5f,
+		collision_.Extents.y * 0.5f,
+		collision_.Extents.z * 0.5f
+	);
+
+	collision_model_ = DX9::Model::CreateBox(
+		DXTK->Device9,
+		collision_.Extents.x,
+		collision_.Extents.y,
+		collision_.Extents.z
+	);
+	D3DMATERIAL9 material{};
+	material.Diffuse = DX9::Colors::Value(0.0f, 0.0f, 1.0f, 0.75f);
+	collision_model_->SetMaterial(material);
+
 
 	font = DX9::SpriteFont::CreateDefaultFont(DXTK->Device9);
 
 	hit_efc_ = DX12Effect.Create(L"Effect/Eff_BossHit/Eff_bossHit.efk");
 }
 
-void BossCore::Update(const float deltaTime, bool core_hit_flag) {
+void BossCore::Update(const float deltaTime, bool core_hit_flag, Boss* boss) {
+	time_delta_ = deltaTime;
 	collision_.Center	   = model->GetPosition();
 	collision_.Orientation = model->GetRotationQuaternion();
+	CoreMove(boss->IsBossWeak());
 	HitPlayerAttack(core_hit_flag);
+}
+
+void BossCore::Render() {
+	BossParts::Render();
+	collision_model_->SetPosition(collision_.Center);
+	collision_model_->SetRotationQuaternion(collision_.Orientation);
+	collision_model_->Draw();
 }
 
 void BossCore::Render2D() {
@@ -31,6 +59,15 @@ void BossCore::Render2D() {
 		DX9::Colors::Red,
 		L"É{ÉX:%i", core_hp_
 	);
+}
+
+void BossCore::CoreMove(bool weak_state){
+	if (weak_state) {
+		position.y = std::min(position.y + CORE_MOVE_SPEED_Y_ * time_delta_, CORE_MOVE_DEST_Y_);
+	}
+	else {
+		position.y = std::max(position.y - CORE_MOVE_SPEED_Y_ * time_delta_, CORE_INTIAL_POS_Y_);
+	}
 }
 
 void BossCore::HitPlayerAttack(bool core_hit_flag) {
@@ -45,6 +82,6 @@ void BossCore::HitPlayerAttack(bool core_hit_flag) {
 
 void BossCore::HitProcessing() {
 	core_hp_--;
-	DX12Effect.Play(hit_efc_, core_pos_);
+	DX12Effect.Play(hit_efc_, position);
 	hit_flag_ = true;
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Core/BossCore.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Core/BossCore.h
@@ -3,10 +3,14 @@
 #include "Classes/Enemy/Boss/Parts/BossParts.h"
 #include "Base/DX12Effekseer.h"
 
+class Boss;
+
 class BossCore : public BossParts {
 public:
 	BossCore() {
 		core_hp_  = 0;
+		time_delta_ = 0.0f;
+		core_weak_time_ = 0.0f;
 		hit_flag_ = false;
 		core_pos_ = SimpleMath::Vector3::Zero;
 		hit_efc_  = nullptr;
@@ -14,23 +18,31 @@ public:
 
 	virtual void Initialize();
 	virtual void LoadAssets();
-	virtual void Update(const float deltaTime, bool core_hit_flag);
+	virtual void Update(const float deltaTime, bool core_hit_flag, Boss* boss);
+	virtual void Render();
 	void Render2D();
 
 	int GetBossHP() { return core_hp_; }
 	BoundingOrientedBox GetCoreCollision() { return collision_; }
 private:
+	void CoreMove(bool weak_state);
 	void HitPlayerAttack(bool core_hit_flag);
 	void HitProcessing();
 
 	BoundingOrientedBox collision_;
+	DX9::MODEL collision_model_;
 	DX9::SPRITEFONT font;
 	EFFECT hit_efc_;
 
 	int core_hp_;
+	float time_delta_;
+	float core_weak_time_;
 	bool hit_flag_;
 	SimpleMath::Vector3 core_pos_;
 
 	const int BOSS_MAX_HP_ = 30;
-	const float BOSS_CORE_POS_Y_ = 9.0f;
+	const float CORE_INTIAL_POS_Y_ = -2.0f;
+	const float CORE_INTIAL_POS_Z_ = 1.0f;
+	const float CORE_MOVE_SPEED_Y_ = 10.0f;
+	const float CORE_MOVE_DEST_Y_ = 1.5f;
 };
