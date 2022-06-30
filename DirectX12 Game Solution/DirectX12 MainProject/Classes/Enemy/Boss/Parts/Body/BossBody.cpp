cd641346921d593813c049e45cec8b143e9ee92c
MSI\jfuki <jfukiage46@gmail.com>
2022-06-30 17:41:48 +0900

    [Update] ボス本体をアニメーションモデル対応にした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
@@ -4,5 +4,6 @@ void BossBody::Initialize() {
 	BossParts::Initialize(SimpleMath::Vector3::Zero, SimpleMath::Vector3::Zero);
 }
 void BossBody::LoadAssets() {
-	BossParts::LoadAssets(L"Boss/boss.X");
+	BossParts::LoadAssets(L"Boss/boss_animation.X");
+	model_->SetTrackEnable(0, true);
 }
