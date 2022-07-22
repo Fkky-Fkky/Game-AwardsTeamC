afb56a4c77c47ded32ea0f93f0d39052e227bc22
MSI\jfuki <jfukiage46@gmail.com>
2022-07-22 15:41:44 +0900

    [Update] 左手叩きつけ攻撃予備動作追加

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftBeat.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftBeat.cpp
@@ -12,6 +12,7 @@ void LeftBeat::Update(const float deltaTime, ObjectManager* obj_m, Boss* boss) {
 	case HAND_CHECK:	HandCheck(boss);		break;
 	case READY:			Ready(obj_m);			break;
 	case ATTACK:		LeftBeatAttack(boss);	break;
+	case WAIT:			Wait();					break;
 	case RETURN:		HandReturn();			break;
 	case ACTION_END:	boss->ActionEnd();		break;
 	}
@@ -26,22 +27,22 @@ void LeftBeat::HandCheck(Boss* boss) {	//手の状態を確認
 	boss_action_state_ = READY;
 }
 
-void LeftBeat::Ready(ObjectManager* obj_m) { //攻撃に必用な変数の設定
-	if (!player_pos_get_flag_) {
-		move_dest_ = obj_m->GetPlayerPos();
-		player_pos_get_flag_ = true;
-	}
+void LeftBeat::Ready(ObjectManager* obj_m) { //プレイヤーの座標に手を移動させる
+	SimpleMath::Vector3 move_dest_ = obj_m->GetPlayerPos();
 
-	pos_.z = std::max(pos_.z - MOVE_SPEED_Z_ * time_delta_, 0.0f);
-
-	if (pos_.x < move_dest_.x) {
-		pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, move_dest_.x);
+	ready_time_ = std::min(ready_time_ + time_delta_, READY_TIME_MAX_);
+	if (ready_time_ <= CHASE_PLAYER_TIME_) {
+		if (pos_.x < move_dest_.x) {
+			pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, move_dest_.x);
+		}
+		else {
+			pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, move_dest_.x);
+		}
 	}
-	else {
-		pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, move_dest_.x);
-	}
+	pos_.z  = std::max(pos_.z  - MOVE_SPEED_Z_ * time_delta_, 0.0f);
+	rote_.x = std::max(rote_.x - ROTE_SPEED_   * time_delta_, -XM_1DIV2PI);
 
-	if (pos_.x == move_dest_.x) {
+	if (ready_time_ >= READY_TIME_MAX_) {
 		boss_action_state_ = ATTACK;
 	}
 }
@@ -49,34 +50,36 @@ void LeftBeat::Ready(ObjectManager* obj_m) { //攻撃に必用な変数の設定
 void LeftBeat::LeftBeatAttack(Boss* boss) { //叩きつけ攻撃
 	boss_handL_->SetAttackFlag(true);
 	beat_time_ += time_delta_;
-	float beat_ = BEAT_SPEED_* beat_time_ - HALF_ * BEAT_GRAVITY_ * beat_time_ * beat_time_;
+	float beat_ = BEAT_SPEED_ * beat_time_ - HALF_ * BEAT_GRAVITY_ * beat_time_ * beat_time_;
 	pos_.y += beat_;
-	rote_.x = std::max(rote_.x - ROTE_SPEED_ * time_delta_, -XM_1DIV2PI);
 
 	if (pos_.y <= 0.0f) {
 		pos_.y  = 0.0f;
 		boss->PlayBeatSE();
 		boss->PlayBeatEffect(pos_);
 		boss_handL_->SetAttackFlag(false);
+		boss_action_state_ = WAIT;
+	}
+}
+
+void LeftBeat::Wait() {	//硬直
+	wait_time_ += time_delta_;
+	if (wait_time_ >= WAIT_TIME_MAX_) {
 		(!hand_state_) ? boss_handL_->SetHandMotion(HAND_MOTION::ROCK_BACK) : boss_handL_->SetHandMotion(HAND_MOTION::PAPER_BACK);
 		boss_action_state_ = RETURN;
 	}
 }
 
 void LeftBeat::HandReturn() { //手を初期位置へ移動
-	wait_time_ += time_delta_;
-
-	if (wait_time_ >= WAIT_TIME_MAX_) {
-		if (pos_.x < HAND_L_INITIAL_POS_X_) {
-			pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
-		}
-		else {
-			pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
-		}
-		pos_.y  = std::min(pos_.y + MOVE_SPEED_Y_ * time_delta_, HAND_INITIAL_POS_Y_);
-		pos_.z  = std::min(pos_.z + MOVE_SPEED_Z_ * time_delta_, HAND_INITIAL_POS_Z_);
-		rote_.x = std::min(rote_.x + ROTE_SPEED_ * time_delta_, XM_PIDIV4);
+	if (pos_.x < HAND_L_INITIAL_POS_X_) {
+		pos_.x = std::min(pos_.x + MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
 	}
+	else {
+		pos_.x = std::max(pos_.x - MOVE_SPEED_X_ * time_delta_, HAND_L_INITIAL_POS_X_);
+	}
+	pos_.y  = std::min(pos_.y + MOVE_SPEED_Y_ * time_delta_, HAND_INITIAL_POS_Y_);
+	pos_.z  = std::min(pos_.z + MOVE_SPEED_Z_ * time_delta_, HAND_INITIAL_POS_Z_);
+	rote_.x = std::min(rote_.x + ROTE_SPEED_  * time_delta_, XM_PIDIV4);
 
 	if (pos_.y >= HAND_INITIAL_POS_Y_ &&
 		pos_.x == HAND_L_INITIAL_POS_X_) {
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftBeat.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/LeftBeat.h
@@ -9,11 +9,10 @@ public:
 		time_delta_ = 0.0f;
 		beat_time_  = 0.0f;
 		wait_time_  = 0.0f;
-		hand_state_			 = false;
-		player_pos_get_flag_ = false;
-		pos_	   = SimpleMath::Vector3::Zero;
-		rote_	   = SimpleMath::Vector3::Zero;
-		move_dest_ = SimpleMath::Vector3::Zero;
+		ready_time_ = 0.0f;
+		hand_state_ = false;
+		pos_  = SimpleMath::Vector3::Zero;
+		rote_ = SimpleMath::Vector3::Zero;
 	}
 	virtual void Update(const float deltaTime, ObjectManager* obj_m, Boss* boss);
 
@@ -21,6 +20,7 @@ private:
 	void HandCheck(Boss* boss);
 	void Ready(ObjectManager* obj_m);
 	void LeftBeatAttack(Boss* boss);
+	void Wait();
 	void HandReturn();
 
 	int boss_action_state_;
@@ -28,13 +28,14 @@ private:
 	float time_delta_;
 	float beat_time_;
 	float wait_time_;
+	float ready_time_;
 
 	bool hand_state_;
-	bool player_pos_get_flag_;
 
 	SimpleMath::Vector3 pos_;
 	SimpleMath::Vector3 rote_;
-	SimpleMath::Vector3 move_dest_;
-
-	const float WAIT_TIME_MAX_ = 0.2f;
+	
+	const float CHASE_PLAYER_TIME_ = 2.0f;
+	const float READY_TIME_MAX_	   = 3.0f;
+	const float WAIT_TIME_MAX_	   = 1.5f;
 };
