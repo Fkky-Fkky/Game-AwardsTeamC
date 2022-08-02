c19c22c12dec6053f727c76f61225222eed337a9
MSI\jfuki <jfukiage46@gmail.com>
2022-08-02 17:20:53 +0900

    [Update] 両手の初期回転値変更

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/RightHand/BossHandR.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/RightHand/BossHandR.cpp
@@ -3,7 +3,7 @@
 void BossHandR::Initialize() {
 	BossHand::Initialize(
 		SimpleMath::Vector3(INITIAL_POS_X, INITIAL_POS_Y, INITIAL_POS_Z),
-		SimpleMath::Vector3(XM_PIDIV4, 5.0f, 0.0f)
+		SimpleMath::Vector3::Zero
 	);
 }
 
