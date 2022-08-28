d02b51eebe1663cc568ad7fee134277075e2f699
MSI\jfuki <jfukiage46@gmail.com>
2022-08-28 11:35:47 +0900

    [Update] ライトの調整

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/My_Light/My_Light.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/My_Light/My_Light.cpp
@@ -3,23 +3,22 @@
 void My_Light::LoadAssets() {
     D3DLIGHT9 light{};
     light.Type     = D3DLIGHT_DIRECTIONAL;
-    light.Ambient  = DX9::Colors::Value(1.0f, 1.0f, 1.0f, 1.0f);
-    light.Diffuse  = DX9::Colors::Value(1.0f, 1.0f, 1.0f, 1.0f);
-    light.Specular = DX9::Colors::Value(1.0f, 1.0f, 1.0f, 1.0f);
+    light.Ambient  = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
+    light.Diffuse  = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
+    light.Specular = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
 
     //1つ目のライト
-    light.Direction = DX9::VectorSet(-LIGHT_POS_X_, LIGHT_POS_Y_, LIGHT_POS_Z_);
+    light.Direction = DX9::VectorSet(-10, -10, 10);
     DXTK->Direct3D9->SetLight(0, light);
 
     //2つ目のライト
-    light.Direction = DX9::VectorSet(LIGHT_POS_X_, LIGHT_POS_Y_, LIGHT_POS_Z_);
+    light.Direction = DX9::VectorSet(10, -10, 10);
     DXTK->Direct3D9->SetLight(1, light);
 
     //3つ目のライト
-    light.Ambient   = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
-    light.Diffuse   = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
-    light.Specular  = DX9::Colors::Value(0.5f, 0.5f, 0.5f, 1.0f);
-
-    light.Direction = DX9::VectorSet(0.0f, -10.0f, 0.0f);
+    light.Ambient  = DX9::Colors::Value(2.0f, 2.0f, 2.0f, 50.0f);
+    light.Diffuse  = DX9::Colors::Value(2.0f, 2.0f, 2.0f, 50.0f);
+    light.Specular = DX9::Colors::Value(2.0f, 2.0f, 2.0f, 50.0f);
+    light.Direction = DX9::VectorSet(0, 10, 10);
     DXTK->Direct3D9->SetLight(2, light);
 }
