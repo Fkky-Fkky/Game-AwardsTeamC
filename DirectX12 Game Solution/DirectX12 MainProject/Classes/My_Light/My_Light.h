2b44d6ec6191681f8e58ef344199c07cbb36aa97
MSI\jfuki <jfukiage46@gmail.com>
2022-08-31 17:58:01 +0900

    [Update] ライトの種類追加。 ライトの位置、色調整

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/My_Light/My_Light.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/My_Light/My_Light.h
@@ -7,11 +7,17 @@ using namespace DirectX;
 
 class My_Light {
 public:
-
+	My_Light() {
+		light_pos_ = SimpleMath::Vector3::Zero;
+	}
+	~My_Light(){};
 	void LoadAssets();
-
+	void Update(const float deltaTime);
 private:
-	const float LIGHT_POS_X_ = 10.0f;
-	const float LIGHT_POS_Y_ = 15.0f;
-	const float LIGHT_POS_Z_ = 3.0f;
+	D3DLIGHT9 light;
+
+	SimpleMath::Vector3 light_pos_{};
+	const float LIGHT_POS_X_ = 0.0f;
+	const float LIGHT_POS_Y_ = 4.0f;
+	const float LIGHT_POS_Z_ = -24.0f;
 };
