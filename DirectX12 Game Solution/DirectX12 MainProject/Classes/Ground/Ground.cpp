ccec1edc683553f3ac3e2fbbb8f4c535209b1459
MSI\jfuki <jfukiage46@gmail.com>
2022-08-31 13:19:48 +0900

    [Update] 背景画像の実装

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
@@ -3,8 +3,11 @@
 
 void Ground::LoadAssets() {
     stage_ = DX9::Model::CreateFromFile(DXTK->Device9, L"Ground/Stage/stage.X");
-    stage_->SetPosition(SimpleMath::Vector3::Zero);
+    stage_->SetPosition(SimpleMath::Vector3(STAGE_POS_X_, 0.0f, STAGE_POS_Z_));
     stage_->SetScale(STAGE_SCALE_);
+
+    bg_   = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/BossBattleBG.png");
+    mist_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/Mist.png");
 
     bgm_main_ = DX9::MediaRenderer::CreateFromFile(DXTK->Device9, L"BGM/game_main.mp3");
     bgm_main_->Play();
@@ -21,5 +24,12 @@ void Ground::Render() {
 }
 
 void Ground::Render2D() {
-
+    DX9::SpriteBatch->DrawSimple(
+        bg_.Get(),
+        SimpleMath::Vector3(0.0f, 0.0f, BOSS_BG_POS_Z_)
+    );
+    DX9::SpriteBatch->DrawSimple(
+        mist_.Get(),
+        SimpleMath::Vector3(mist_pos_.x, mist_pos_.y, MIST_POS_Z_)
+    );
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
@@ -9,7 +9,9 @@ using namespace DirectX;
 
 class Ground {
 public:
-	Ground() {};
+	Ground() { 
+		mist_pos_ = SimpleMath::Vector3::Zero; 
+	}
 	~Ground() {};
 
 	void LoadAssets();
@@ -19,6 +21,14 @@ public:
 
 private:
 	DX9::MODEL stage_;
+	DX9::SPRITE bg_;
+	DX9::SPRITE mist_;
 	DX9::MEDIARENDERER bgm_main_;
-	const float STAGE_SCALE_ = 2.0f;
+
+	SimpleMath::Vector3 mist_pos_;
+	const float STAGE_POS_X_ = 35.0f;
+	const float STAGE_POS_Z_ = -40.0f;
+	const float STAGE_SCALE_ = 7.0f;
+	const float BOSS_BG_POS_Z_ = 10000.0f;
+	const float MIST_POS_Z_ = 9999.0f;
 };
