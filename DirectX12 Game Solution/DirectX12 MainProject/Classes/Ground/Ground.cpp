1347f91ec6a5b2bbd57057e369ac5b9ad208b10a
MSI\jfuki <jfukiage46@gmail.com>
2022-09-01 12:23:15 +0900

    [Update] 背景の霧がスクロールするようにした 背景の画像が切り替わるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.cpp
@@ -6,7 +6,8 @@ void Ground::LoadAssets() {
     stage_->SetPosition(SimpleMath::Vector3(STAGE_POS_X_, 0.0f, STAGE_POS_Z_));
     stage_->SetScale(STAGE_SCALE_);
 
-    bg_   = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/BossBattleBG.png");
+    enemy_bg_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/EnemyBG.png");
+    boss_bg_  = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/BossBattleBG.png");
     mist_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Ground/BG/Mist.png");
 
     bgm_main_ = DX9::MediaRenderer::CreateFromFile(DXTK->Device9, L"BGM/game_main.mp3");
@@ -17,6 +18,32 @@ void Ground::Update(const float deltaTime, ObjectManager* object) {
     if (bgm_main_->isComplete()) {
         bgm_main_->Replay();
     }
+
+    //if (!bg_flag_) {
+    //    bg_flag_ = true;
+    //}
+    //else {
+    //    bg_flag_ = false;
+    //}
+    
+    if (bg_flag_) {
+        enemy_bg_alpha_ = std::max(enemy_bg_alpha_ - ADD_ALPHA_SPEED_ * deltaTime, 0.0f);
+    }
+    else {
+        enemy_bg_alpha_ = std::min(enemy_bg_alpha_ + ADD_ALPHA_SPEED_ * deltaTime, COLOR_MAX_);
+    }
+
+    if (bg_flag_) {
+        miset_speed_ = std::min(miset_speed_ + ADD_MIST_SPEED_ * deltaTime, MIST_MOVE_SPEED_QUICK_);
+    }
+    else {
+        miset_speed_ = std::max(miset_speed_ - ADD_MIST_SPEED_ * deltaTime, MIST_MOVE_SPEED_SLOW_);
+    }
+
+    mist_pos_.x += miset_speed_ * deltaTime;
+    if (mist_pos_.x >= 0.0f) {
+        mist_pos_.x  = MIST_INIT_POS_X_;
+    }
 }
 
 void Ground::Render() {
@@ -25,11 +52,19 @@ void Ground::Render() {
 
 void Ground::Render2D() {
     DX9::SpriteBatch->DrawSimple(
-        bg_.Get(),
+        enemy_bg_.Get(),
+        SimpleMath::Vector3(0.0f, 0.0f, ENEMY_BG_POS_Z_),
+        RectWH(0, 0, SPRITE_WIDTH_, SPRITE_HIGHT_),
+        DX9::Colors::RGBA(COLOR_MAX_, COLOR_MAX_, COLOR_MAX_, (int)enemy_bg_alpha_)
+    );
+
+    DX9::SpriteBatch->DrawSimple(
+        boss_bg_.Get(),
         SimpleMath::Vector3(0.0f, 0.0f, BOSS_BG_POS_Z_)
     );
+    
     DX9::SpriteBatch->DrawSimple(
         mist_.Get(),
-        SimpleMath::Vector3(mist_pos_.x, mist_pos_.y, MIST_POS_Z_)
+        SimpleMath::Vector3(mist_pos_)
     );
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Ground/Ground.h
@@ -10,7 +10,10 @@ using namespace DirectX;
 class Ground {
 public:
 	Ground() { 
-		mist_pos_ = SimpleMath::Vector3::Zero; 
+		mist_pos_ = SimpleMath::Vector3(MIST_INIT_POS_X_,0.0f, MIST_INIT_POS_Z_);
+		bg_flag_ = false;
+		enemy_bg_alpha_ = COLOR_MAX_;
+		miset_speed_ = MIST_MOVE_SPEED_SLOW_;
 	}
 	~Ground() {};
 
@@ -21,14 +24,28 @@ public:
 
 private:
 	DX9::MODEL stage_;
-	DX9::SPRITE bg_;
+	DX9::SPRITE boss_bg_;
+	DX9::SPRITE enemy_bg_;
 	DX9::SPRITE mist_;
 	DX9::MEDIARENDERER bgm_main_;
 
 	SimpleMath::Vector3 mist_pos_;
+	bool bg_flag_;
+	float enemy_bg_alpha_;
+	float miset_speed_;
+
 	const float STAGE_POS_X_ = 35.0f;
 	const float STAGE_POS_Z_ = -40.0f;
 	const float STAGE_SCALE_ = 7.0f;
 	const float BOSS_BG_POS_Z_ = 10000.0f;
-	const float MIST_POS_Z_ = 9999.0f;
+	const float ENEMY_BG_POS_Z_ = 9999.0f;
+	const float MIST_INIT_POS_Z_ = 9998.0f;
+	const float MIST_INIT_POS_X_ = -1280.0f;
+	const float MIST_MOVE_SPEED_SLOW_ = 100.0f;
+	const float MIST_MOVE_SPEED_QUICK_ = 200.0f;
+	const float ADD_MIST_SPEED_ = 80.0f;
+	const float ADD_ALPHA_SPEED_ = 300.0f;
+	const float COLOR_MAX_ = 255.0f;
+	const float SPRITE_WIDTH_ = 1280.0f;
+	const float SPRITE_HIGHT_ = 720.0f;
 };
