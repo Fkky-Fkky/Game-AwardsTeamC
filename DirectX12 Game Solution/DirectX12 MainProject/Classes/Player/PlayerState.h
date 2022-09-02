5ba2b2b8be4c01d607fc114f174877ef57f83fdf
MSI\jfuki <jfukiage46@gmail.com>
2022-09-02 18:45:52 +0900

    [Update] プレイヤーの移動範囲調整

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerState.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerState.h
@@ -16,5 +16,5 @@ protected:
 	const float PLAYER_MOVE_SPEED_ = 10.0f;
 	const float PLAYER_ROTATION_ANGLE_ = 90.0f;
 	const float PLAYER_LIMIT_MAX_POS_X_ = 17.0f;
-	const float PLAYER_LIMIT_MIN_POS_X_ = -14.0f;
+	const float PLAYER_LIMIT_MIN_POS_X_ = -17.0f;
 };
