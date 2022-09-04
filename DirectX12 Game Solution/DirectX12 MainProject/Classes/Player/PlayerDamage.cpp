6a2b894a1cc47dcf550a934e0eb30663f8bb0841
MSI\jfuki <jfukiage46@gmail.com>
2022-09-04 10:54:27 +0900

    [Update] プレイヤー被ダメージ時ノックバック実装

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.cpp
@@ -3,7 +3,9 @@
 
 void PlayerDamage::Initialize() {
 	dmg_time = 0.0f;
+	knock_back_dest_ = 0.0f;
 	hit_flag_ = false;
+	one_process_flag_ = false;
 }
 
 void PlayerDamage::Update(const float deltaTime, Player& player) {
@@ -21,6 +23,19 @@ void PlayerDamage::Update(const float deltaTime, Player& player) {
 		player.SetStopInitializeFlag(true);
 		HitProcessing();
 	}
+	
+	if (!one_process_flag_) {
+		knock_back_dest_ = (rot_.y <= -PLAYER_ROTATION_ANGLE_) ? pos_.x - ADD_DEST_ : pos_.x + ADD_DEST_;
+		knock_back_dest_ = std::clamp(knock_back_dest_, PLAYER_LIMIT_MIN_POS_X_, PLAYER_LIMIT_MAX_POS_X_);
+		one_process_flag_ = true;
+	}
+
+	if (pos_.x >= knock_back_dest_) {
+		pos_.x = std::max(pos_.x - KNOCK_BACK_SPEED_ * deltaTime, knock_back_dest_);
+	}
+	else {
+		pos_.x = std::min(pos_.x + KNOCK_BACK_SPEED_ * deltaTime, knock_back_dest_);
+	}
 
 	if (pos_.y >= 0.0f) {
 		pos_.y = std::max(pos_.y - FALL_SPEED_ * deltaTime, 0.0f);
@@ -31,6 +46,6 @@ void PlayerDamage::Update(const float deltaTime, Player& player) {
 }
 
 void PlayerDamage::HitProcessing() {
-	player_hp_ -= 1.0f;
+	player_hp_--;
 	hit_flag_ = true;
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerDamage.h
@@ -7,7 +7,9 @@ public:
 	PlayerDamage() {
 		player_hp_ = PLAYER_HP_MAX_;
 		dmg_time   = 0.0f;
+		knock_back_dest_ = 0.0f;
 		hit_flag_  = false;
+		one_process_flag_ = false;
 	}
 	~PlayerDamage() {};
 
@@ -21,11 +23,14 @@ private:
 
 	float player_hp_;
 	float dmg_time;
+	float knock_back_dest_;
 	bool hit_flag_;
+	bool one_process_flag_;
 
 	SimpleMath::Vector3 pos_;
 	SimpleMath::Vector3 rot_;
-
+	const float ADD_DEST_ = 3.0f;
+	const float KNOCK_BACK_SPEED_ = 20.0f;
 	const float PLAYER_HP_MAX_ = 30.0f;
 	const float FALL_SPEED_	   = 15.0f;
 	const float DMG_TIME_MAX_  = 0.6f;
