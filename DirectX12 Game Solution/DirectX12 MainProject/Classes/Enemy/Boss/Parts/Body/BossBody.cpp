5e9261c77a3e391ee71fd3a3fcd020226a6ec200
MSI\jfuki <jfukiage46@gmail.com>
2022-09-08 20:08:42 +0900

    [Update] Weak状態の動き作成

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Body/BossBody.cpp
@@ -1,11 +1,17 @@
 #include "Classes/Enemy/Boss/Parts/Body/BossBody.h"
 #include "Classes/Collision/ObjectManager.h"
+#include "Classes/Enemy/Boss/Boss.h"
 
 void BossBody::Initialize() {
 	BossParts::Initialize(
 		SimpleMath::Vector3(0.0f, BODY_INIT_POS_Y_, BODY_INIT_POS_Z_),
 		SimpleMath::Vector3(BODY_INIT_ROT_X_, 0.0f, 0.0f)
 	);
+	time_delta_ = 0.0f;
+	shake_time_ = 0.0f;
+	entyoku_time_ = 0.0f;
+	is_weak_ = false;
+	shake_set_flag_ = false;
 }
 
 void BossBody::LoadAssets() {
@@ -30,10 +36,22 @@ void BossBody::LoadAssets() {
 	model_->SetTrackEnable(0, true);
 }
 
-void BossBody::Update(const float deltaTime, const ObjectManager* const obj_m_) {
+void BossBody::Update(const float deltaTime, const ObjectManager* const obj_m, Boss* const boss) {
 	BossParts::Update(deltaTime);
-	SimpleMath::Vector3 player_pos_ = obj_m_->GetPlayerPos();
+	time_delta_ = deltaTime;
+	is_weak_ = obj_m->IsBossWeak();
+	SimpleMath::Vector3 player_pos_ = obj_m->GetPlayerPos();
 	rotation.y = player_pos_.x * -1;
+	WeakAction();
+	
+	if (is_shake_) {
+		shake_time_ = std::max(shake_time_ - deltaTime, 0.0f);
+	}
+	if (shake_time_ <= 0.0f) {
+		is_shake_ = false;
+	}
+
+	boss->SetVerticalShake(is_shake_);
 	body_coll_.Center = model_->GetPosition();
 	body_coll_.Orientation = model_->GetRotationQuaternion();
 }
@@ -42,5 +60,36 @@ void BossBody::Render() const {
 	BossParts::Render();
 	coll_model_->SetPosition(body_coll_.Center);
 	coll_model_->SetRotationQuaternion(body_coll_.Orientation);
-	coll_model_->Draw();
+	//coll_model_->Draw();
+}
+
+void BossBody::WeakAction() {
+	const float BODY_UP_SPEED_Y_   = 10.0f;
+	const float BODY_DOWN_SPEED_Y_ = 40.0f;
+	if (is_weak_) {
+		const float SHAKE_TIME_MAX_ = 0.2f;
+		if (position.y > BODY_DOWN_POS_Y_) {
+			entyoku_time_ += time_delta_;
+			float entyoku = 10.0f * entyoku_time_ - 0.5f * 50.0f * entyoku_time_ * entyoku_time_;
+			position.y += entyoku;
+		}
+
+		//position.y = std::max(position.y - BODY_DOWN_SPEED_Y_ * time_delta_, BODY_DOWN_POS_Y_);
+		position.z = std::max(position.z - BODY_DOWN_SPEED_Y_ * time_delta_, 5.0f);
+		rotation.x = 0.0f;
+		rotation.y = 0.0f;
+		if (!shake_set_flag_ && position.y <= BODY_DOWN_POS_Y_) {
+			position.y = BODY_DOWN_POS_Y_;
+			shake_set_flag_ = true;
+			is_shake_		= true;
+			shake_time_ = SHAKE_TIME_MAX_;
+		}
+	}
+	else {
+		position.y = std::min(position.y + BODY_UP_SPEED_Y_ * time_delta_, BODY_INIT_POS_Y_);
+		position.z = std::min(position.z + BODY_DOWN_SPEED_Y_ * time_delta_, BODY_INIT_POS_Z_);
+		rotation.x = std::max(rotation.x - 10.0f, BODY_INIT_ROT_X_);
+		shake_set_flag_ = false;
+		entyoku_time_ = 0.0f;
+	}
 }
