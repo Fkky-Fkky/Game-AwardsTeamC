732fd00950ddc58755a9b3b9f010be2b29908cd3
MSI\jfuki <jfukiage46@gmail.com>
2022-09-12 17:52:41 +0900

    [Update] Weak状態の時間をObjectManagerからとるようにした

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Weak.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Weak.cpp
@@ -1,5 +1,6 @@
 #include "Classes/Enemy/Boss/Parts/Hands/Attack/Weak.h"
 #include "Classes/Enemy/Boss/Parts/Hands/HandManager.h"
+#include "Classes/Collision/ObjectManager.h"
 
 void Weak::Update(const float deltaTime, const ObjectManager* const obj_m, HandManager* const hand_m) {
 	pos_r_ = boss_handR_->GetHandPos();
@@ -7,7 +8,7 @@ void Weak::Update(const float deltaTime, const ObjectManager* const obj_m, HandM
 
 	time_delta_ = deltaTime;
 	switch (weak_state_) {
-	case WEAK:			   BossWeak(hand_m);	break;
+	case WEAK:			   BossWeak(obj_m);	break;
 	case INITIAL_POS_MOVE: InitialPosMove();	break;
 	case ACTION_END:	   hand_m->ActionEnd(); break;
 	}
@@ -16,11 +17,10 @@ void Weak::Update(const float deltaTime, const ObjectManager* const obj_m, HandM
 	boss_handL_->SetHandPos(pos_l_);
 }
 
-void Weak::BossWeak(HandManager* const hand_m) {	//ウィーク状態維持
-	hand_m->SetWeakState(true);
-	weak_time_ = std::min(weak_time_ + time_delta_, WEAK_TIME_MAX_);
-	if (weak_time_ >= WEAK_TIME_MAX_) {
-		hand_m->SetWeakState(false);
+void Weak::BossWeak(const ObjectManager* const obj_m) {	//ウィーク状態維持
+	boss_handL_->SetAttackFlag(false);
+	boss_handR_->SetAttackFlag(false);
+	if (!obj_m->IsBossWeak()) {
 		weak_state_ = INITIAL_POS_MOVE;
 	}
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Weak.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Weak.h
@@ -7,7 +7,6 @@ public:
 	Weak() {
 		weak_state_ = WEAK;
 		time_delta_ = 0.0f;
-		weak_time_	= 0.0f;
 		pos_r_ = SimpleMath::Vector3::Zero;
 		pos_l_ = SimpleMath::Vector3::Zero;
 	}
@@ -15,13 +14,12 @@ public:
 	virtual void Update(const float deltaTime, const ObjectManager* const obj_m, HandManager* const hand_m);
 
 private:
-	void BossWeak(HandManager* const hand_m);
+	void BossWeak(const ObjectManager* const obj_m);
 	void InitialPosMove();
 
 	int weak_state_;
 
 	float time_delta_;
-	float weak_time_;
 
 	SimpleMath::Vector3 pos_r_;
 	SimpleMath::Vector3 pos_l_;
@@ -31,6 +29,4 @@ private:
 		INITIAL_POS_MOVE,
 		ACTION_END
 	};
-
-	const float WEAK_TIME_MAX_ = 6.0f;
 };
