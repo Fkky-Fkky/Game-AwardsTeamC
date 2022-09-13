085b56a012a99b2555c3b0f20ebc964d5d6ec7ee
MSI\jfuki <jfukiage46@gmail.com>
2022-09-13 17:40:43 +0900

    [Update] プレイヤーの攻撃当たり判定調整

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAttackColision.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Player/PlayerAttackColision.h
@@ -33,11 +33,11 @@ private:
 	const float RIGHT_ANGLE_ = 0.0f;
 	const float LEFT_ANGLE_  = 200.0f;
 	const float MAX_ATTACK_TIME_ = 3.3f;
-	const float ATTACK_DISTANCE_X_ = 1.5f;
+	const float ATTACK_DISTANCE_X_ = 4.0f;
 	const float ATTACK_DISTANCE_Y_ = 1.5f;
 	const float ATTACK_START_TIME_ = 1.3f;
 	const float ATTACK_END_TIME_ = 1.7f;
-	const float COLLISION_SIZE_MULTIPLY_X_ = 5.0f;
+	const float COLLISION_SIZE_MULTIPLY_X_ = 0.1f;
 	const float COLLISION_SIZE_MULTIPLY_Y_ = 2.5f;
-	const float COLLISION_SIZE_MULTIPLY_Z_ = 3.0f;
+	const float COLLISION_SIZE_MULTIPLY_Z_ = 4.0f;
 };
