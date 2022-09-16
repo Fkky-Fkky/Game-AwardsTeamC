f4a9289922fc7ae1a376d4de9ee9c96d73a0efe3
MSI\jfuki <jfukiage46@gmail.com>
2022-09-16 16:30:46 +0900

    [Add] Adventクラス追加

---
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Advent.cpp
@@ -0,0 +1,9 @@
+#include "Classes/Enemy/Boss/Parts/Hands/Attack/Advent.h"
+
+void boss::Advent::Update(const float deltaTime, const ObjectManager* const obj_m, HandManager* const hand_m) {
+
+}
+
+void boss::Advent::AdventAction() {
+
+}
