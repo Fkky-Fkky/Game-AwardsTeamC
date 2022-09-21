71f08dadf3d384f6d63f50bc501d93a3657b62bf
MSI\jfuki <jfukiage46@gmail.com>
2022-09-21 16:26:30 +0900

    [Update] 各Slapクラス仮想関数使用。それに伴い不要な変数・定数削除

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/DoubleSlap.h
@@ -8,11 +8,9 @@ namespace boss {
 		DoubleSlap() {
 			action_state_ = HAND_CHECK;
 			time_delta_ = 0.0f;
-			r_slap_speed_x_ = 0.0f;
-			r_slap_time_y_ = 0.0f;
+			slap_y_		= 0.0f;
 			l_slap_speed_x_ = 0.0f;
-			l_slap_time_y_ = 0.0f;
-			wait_time_ = 0.0f;
+			wait_time_		= 0.0f;
 			ready_end_r_ = false;
 			ready_end_l_ = false;
 			atk_end_r_ = false;
@@ -21,9 +19,7 @@ namespace boss {
 			return_end_l_ = false;
 			hand_state_ = false;
 			r_pos_ = SimpleMath::Vector3::Zero;
-			r_rote_ = SimpleMath::Vector3::Zero;
 			l_pos_ = SimpleMath::Vector3::Zero;
-			l_rote_ = SimpleMath::Vector3::Zero;
 		}
 
 		~DoubleSlap() {};
@@ -35,18 +31,16 @@ namespace boss {
 		void ReadyR();
 		void ReadyL();
 		void Attack(HandManager* const hand_m);
-		void SlapR(HandManager* const had_m);
-		void SlapL();
+		void SlapR(HandManager* const hand_m);
+		void SlapL(HandManager* const hand_m);
 		void Reset();
 		void HandReturn();
 
 		int action_state_;
 
 		float time_delta_;
-		float r_slap_speed_x_;
-		float r_slap_time_y_;
+		float slap_y_;
 		float l_slap_speed_x_;
-		float l_slap_time_y_;
 		float wait_time_;
 
 		bool ready_end_r_;
@@ -58,14 +52,10 @@ namespace boss {
 		bool hand_state_;
 
 		SimpleMath::Vector3 r_pos_;
-		SimpleMath::Vector3 r_rote_;
 		SimpleMath::Vector3 l_pos_;
-		SimpleMath::Vector3 l_rote_;
 
 		const float L_HAND_DEST_Y_ = 8.0f;
 		const float ATTACK_START_TIME_R_ = 1.0f;
 		const float WAIT_TIME_MAX_ = 1.5f;
-		const float ADD_SPEED_ = 40.0f;
-		const float SLAP_SPEED_MAX_ = 80.0f;
 	};
 }
