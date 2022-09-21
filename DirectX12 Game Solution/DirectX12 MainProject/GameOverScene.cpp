72b7af6e1d51ce06bd4594bddac143989a0bb5f7
MSI\jfuki <jfukiage46@gmail.com>
2022-09-21 18:07:08 +0900

    [Update] 各シーンSceneBaseクラス対応

--- DirectX12 Game Solution/DirectX12 MainProject/GameOverScene.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/GameOverScene.cpp
@@ -21,25 +21,7 @@ void GameOverScene::Initialize()
 // Allocate all memory the Direct3D and Direct2D resources.
 void GameOverScene::LoadAssets()
 {
-    descriptorHeap = DX12::CreateDescriptorHeap(DXTK->Device, 1);
-
-    ResourceUploadBatch resourceUploadBatch(DXTK->Device);
-    resourceUploadBatch.Begin();
-
-    RenderTargetState rtState(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
-    SpriteBatchPipelineStateDescription pd(rtState, &CommonStates::NonPremultiplied);
-    D3D12_VIEWPORT viewport = {
-        0.0f, 0.0f, static_cast<int>(screen::width), static_cast<int>(screen::height),
-        D3D12_MIN_DEPTH, D3D12_MAX_DEPTH
-    };
-
-    spriteBatch = DX12::CreateSpriteBatch(DXTK->Device, resourceUploadBatch, pd, &viewport);
-
-    dx9GpuDescriptor = DXTK->Direct3D9->CreateShaderResourceView(descriptorHeap.get(), 0);
-
-    auto uploadResourcesFinished = resourceUploadBatch.End(DXTK->CommandQueue);
-    uploadResourcesFinished.wait();
-
+    scene_base_.LoadAssets();
     // グラフィックリソースの初期化処理
     game_over_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Scene/GameOver.png");
 }
@@ -97,21 +79,7 @@ void GameOverScene::Render()
     DX9::SpriteBatch->End();
     DXTK->Direct3D9->EndScene();
 
-	DXTK->Direct3D9->UpdateResource();
-
-    DXTK->ResetCommand();
-    DXTK->ClearRenderTarget(Colors::CornflowerBlue);
-
-    const auto heapes = descriptorHeap->Heap();
-    DXTK->CommandList->SetDescriptorHeaps(1, &heapes);
-
-    spriteBatch->Begin(DXTK->CommandList);
-    spriteBatch->Draw(
-        dx9GpuDescriptor,
-        XMUINT2(static_cast<int>(screen::width), static_cast<int>(screen::height)),
-        SimpleMath::Vector2(0.0f, 0.0f)
-    );
-    spriteBatch->End();
+    scene_base_.Render();
 
     DXTK->Direct3D9->WaitUpdate();
     DXTK->ExecuteCommandList();
