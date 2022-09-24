2df38fcf56557eedfeac7dbcc4603e4660d514b1
MSI\jfuki <jfukiage46@gmail.com>
2022-09-24 15:35:03 +0900

    [Update] BossHandR,BossHandLクラスDoxygenコメント記述

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/LeftHand/BossHandL.h
@@ -1,15 +1,27 @@
+/**
+* @file BossHandL.h
+* @brief ボスの左手処理クラス
+* @author 吹上純平
+*/
+
 #pragma once
 
+/**
+* インクルードファイル
+*/
 #include "Classes/Enemy/Boss/Parts/Hands/BossHand.h"
 
 namespace boss {
+	/**
+	* @brief BossHandLクラス定義
+	*/
 	class BossHandL : public BossHand {
 	public:
 		void Initialize();
 		void LoadAssets();
 		virtual void Update(const float deltaTime);
 	private:
-		const float INITIAL_POS_X_ = 13.0f;
-		const float ADD_COLLISION_POS_X_ = 1.0f;
+		const float INITIAL_POS_X_ = 13.0f; /**< 初期X座標 */
+		const float ADD_COLLISION_POS_X_ = 1.0f; /**< コリジョンのX座標 */
 	};
 }
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/RightHand/BossHandR.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/RightHand/BossHandR.cpp
@@ -1,5 +1,8 @@
 #include "Classes/Enemy/Boss/Parts/Hands/RightHand/BossHandR.h"
 
+/**
+* @brief 座標と回転の初期化
+*/
 void boss::BossHandR::Initialize() {
 	BossParts::Initialize(
 		SimpleMath::Vector3(INITIAL_POS_X_, INITIAL_POS_Y_, INITIAL_POS_Z_),
@@ -7,12 +10,20 @@ void boss::BossHandR::Initialize() {
 	);
 }
 
+/**
+* @brief モデルの読み込み
+*/
 void boss::BossHandR::LoadAssets() {
 	BossHand::LoadAssets(L"Boss/boss_hand_R.X");
 }
 
+/**
+* @brief コリジョンの更新
+* 
+* @param[in] deltaTime 時間
+*/
 void boss::BossHandR::Update(const float deltaTime) {
 	BossHand::Update(deltaTime);
-	collision_.Center	  = model_->GetPosition() + SimpleMath::Vector3(ADD_COLLISION_POS_X_, 0.0f, 0.0f);
+	collision_.Center	   = model_->GetPosition() + SimpleMath::Vector3(ADD_COLLISION_POS_X_, 0.0f, 0.0f);
 	collision_.Orientation = model_->GetRotationQuaternion();
 }
