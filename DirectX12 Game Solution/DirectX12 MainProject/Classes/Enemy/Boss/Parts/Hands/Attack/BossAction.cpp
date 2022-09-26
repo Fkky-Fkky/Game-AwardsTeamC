9a08869dd13e1a44de5368c3489685b9d45e7d31
MSI\jfuki <jfukiage46@gmail.com>
2022-09-26 11:33:13 +0900

    [Update] BossActionクラスDoxygenコメント記述

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/BossAction.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/BossAction.cpp
@@ -1,15 +1,31 @@
 #include "Classes/Enemy/Boss/Parts/Hands/Attack/BossAction.h"
 #include "Classes/Enemy/Boss/Parts/Hands/ActionManager.h"
 
-void boss::BossAction::Initialize(BossHand* const boss_handL, BossHand* const bosshandR) {
+/**
+* @brief 値の初期化
+* 
+* @param[out] boss_handL 左手
+* @param[out] boss_handR 右手
+*/
+void boss::BossAction::Initialize(BossHand* const boss_handL, BossHand* const boss_handR) {
 	boss_handL_ = boss_handL;
-	boss_handR_ = bosshandR;
+	boss_handR_ = boss_handR;
 	slap_speed_  = 0.0f;
 	slap_y_time_ = 0.0f;
 	is_se_play_	  = false;
 	is_shake_set_ = false;
 }
 
+/**
+* @brief 薙ぎ払い攻撃の仮想関数
+* 
+* - カメラを縦揺れさせる
+* - 薙ぎ払い攻撃に使用するスピードを上げる
+* - SEを再生する
+* 
+* @param[in] deltaTime 時間
+* @param[out] act_m アクションマネージャー
+*/
 void boss::BossAction::SlapAttackBase(const float deltaTime, ActionManager* const act_m) {
 	if (!is_shake_set_) {
 		act_m->SetSideShake(true);
@@ -22,6 +38,12 @@ void boss::BossAction::SlapAttackBase(const float deltaTime, ActionManager* cons
 	}
 }
 
+/**
+* @brief 薙ぎ払い攻撃構え仮想関数
+* 
+* @param[in] deltaTime 時間
+* @return 構えの動き(鉛直投げ上げ)
+*/
 float boss::BossAction::SlapY(const float deltaTime) {
 	slap_y_time_ += deltaTime;
 	float r_slap_y_ = SLAP_SPEED_Y_ * slap_y_time_ - HALF_ * SLAP_GRAVITY_Y_ * slap_y_time_ * slap_y_time_;
