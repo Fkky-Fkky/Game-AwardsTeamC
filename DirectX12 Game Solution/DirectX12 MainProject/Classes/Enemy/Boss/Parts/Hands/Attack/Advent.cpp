83dccb2e99b10b00273e394462cb67a3b7a481e3
MSI\jfuki <jfukiage46@gmail.com>
2022-09-26 11:58:15 +0900

    [Update] AdventクラスDoxygenコメント記述

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Advent.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Advent.cpp
@@ -1,6 +1,13 @@
 #include "Classes/Enemy/Boss/Parts/Hands/Attack/Advent.h"
 #include "Classes/Enemy/Boss/Parts/Hands/ActionManager.h"
 
+/**
+* @brief 手の降臨処理更新
+* 
+* @param[in] deltaTIme 時間
+* @oaram[out] obj_m オブジェクトマネージャー
+* @oaram[out] act_m アクションマネージャー
+*/
 void boss::Advent::Update(const float deltaTime, const ObjectManager* const obj_m, ActionManager* const act_m) {
 	l_pos_ = boss_handL_->GetHandPos();
 	r_pos_ = boss_handR_->GetHandPos();
@@ -19,7 +26,10 @@ void boss::Advent::Update(const float deltaTime, const ObjectManager* const obj_
 	boss_handL_->SetHandRote(l_rot_);
 }
 
-void boss::Advent::AdventAction() {	//手を降臨させる
+/**
+* @brief 手を降臨させる
+*/
+void boss::Advent::AdventAction() {
 	l_pos_.y = std::max(l_pos_.y - MOVE_SPEED_Y_ * time_delta_, HAND_INITIAL_POS_Y_);
 	r_pos_.y = std::max(r_pos_.y - MOVE_SPEED_Y_ * time_delta_, HAND_INITIAL_POS_Y_);
 
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Advent.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/Enemy/Boss/Parts/Hands/Attack/Advent.h
@@ -1,8 +1,19 @@
+/**
+* @file Advent.h
+* @brief ƒ{ƒX‚Ìè~—Õˆ—ƒNƒ‰ƒX
+*/
+
 #pragma once
 
+/**
+* ƒCƒ“ƒNƒ‹[ƒhƒtƒ@ƒCƒ‹
+*/
 #include "Classes/Enemy/Boss/Parts/Hands/Attack/BossAction.h"
 
 namespace boss {
+	/**
+	* @brief AdventƒNƒ‰ƒX’è‹`
+	*/
 	class Advent : public BossAction {
 	public:
 		Advent() {
@@ -19,17 +30,18 @@ namespace boss {
 	private:
 		void AdventAction();
 
-		int advent_state_;
-		float time_delta_;
+		int advent_state_; /**< ~—Õs“®Ši”[ */
+		float time_delta_; /**< ŠÔŠi”[ */
 
-		SimpleMath::Vector3 l_pos_;
-		SimpleMath::Vector3 r_pos_;
-		SimpleMath::Vector3 l_rot_;
-		SimpleMath::Vector3 r_rot_;
+		SimpleMath::Vector3 l_pos_; /**< ¶è‚ÌÀ•WŠi”[ */
+		SimpleMath::Vector3 r_pos_; /**< ‰Eè‚ÌÀ•WŠi”[ */
+		SimpleMath::Vector3 l_rot_; /**< ¶è‚Ì‰ñ“]Ši”[ */
+		SimpleMath::Vector3 r_rot_; /**< ‰Eè‚Ì‰ñ“]Ši”[ */
 
+		/**< ~—Õs“®‚Ìí—Ş */
 		enum ADVENT_STATE_ {
-			ADVENT_ACTION,
-			ACTION_END
+			ADVENT_ACTION, /**< ~—Õ */
+			ACTION_END /**< s“®I—¹ */
 		};
 	};
 }
