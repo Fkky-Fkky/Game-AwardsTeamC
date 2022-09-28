0c69761a80cabba38f2af4ac86bfae21780009a5
MSI\jfuki <jfukiage46@gmail.com>
2022-09-28 23:40:32 +0900

    [Update] タイトル画面に動きをいれた

--- DirectX12 Game Solution/DirectX12 MainProject/TitleScene.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/TitleScene.cpp
@@ -9,14 +9,16 @@
 // Initialize member variables.
 TitleScene::TitleScene()
 {
-    black_alpha_ = 0.0f;
+    text_alpha_ = 0;
+    black_alpha_ = 0;
     start_flag_ = false;
 }
 
 // Initialize a variable and audio resources.
 void TitleScene::Initialize()
 {
-    black_alpha_ = 0.0f;
+    text_alpha_ = 0;
+    black_alpha_ = 0;
     start_flag_ = false;
 }
 
@@ -26,8 +28,9 @@ void TitleScene::LoadAssets()
     scene_base_.LoadAssets();
     // グラフィックリソースの初期化処理
 
-    scene_base_.LoadBG(L"Scene/Title.png");
+    scene_base_.LoadBG(L"Scene/TitleBG.png");
     black_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Scene/Black.png");
+    title_text_ = DX9::Sprite::CreateFromFile(DXTK->Device9, L"Scene/TitleText.png");
     scene_base_.SetBGM(L"BGM_SE/BGM/Title.mp3");
 }
 
@@ -60,16 +63,24 @@ NextScene TitleScene::Update(const float deltaTime)
 
     scene_base_.PlayBGM();
 
+    text_alpha_ += TEXT_ALPHA_SPEED_ * deltaTime;
+    if (text_alpha_ > COLOR_MAX_) {
+        text_alpha_ = COLOR_MAX_;
+    }
+
     if (DXTK->KeyEvent->pressed.Enter) {
         start_flag_ = true;
     }
 
     if (start_flag_) {
-        black_alpha_ = std::min(black_alpha_ + ALPHA_SPEED_ * deltaTime, ALPHA_MAX_);
+        black_alpha_ += BLACK_ALPHA_SPEED_ * deltaTime;
+        if (black_alpha_ > COLOR_MAX_) {
+            black_alpha_ = COLOR_MAX_;
+        }
         scene_base_.FadeOut(deltaTime);
     }
 
-    if (black_alpha_ >= ALPHA_MAX_) {
+    if (black_alpha_ >= COLOR_MAX_) {
         return NextScene::MainScene;
     }
 
@@ -88,10 +99,18 @@ void TitleScene::Render()
     scene_base_.RenderBG();
 
     DX9::SpriteBatch->DrawSimple(
+        title_text_.Get(),
+        SimpleMath::Vector3(TITLE_TEXT_POS_X_, TITLE_TEXT_POS_Y_, 0.0f),
+        RectWH(0, 0, TITLE_TEXT_WIDTH_, TITLE_TEXT_HIGHT_),
+        DX9::Colors::RGBA(COLOR_MAX_, COLOR_MAX_, COLOR_MAX_, text_alpha_),
+        SimpleMath::Vector3(TITLE_TEXT_ORIGIN_X_, TITLE_TEXT_ORIGIN_Y_, 0.0f)
+    );
+
+    DX9::SpriteBatch->DrawSimple(
         black_.Get(),
         SimpleMath::Vector3::Zero,
         RectWH(0, 0, WIDTH_MAX_, HIGHT_MAX_),
-        DX9::Colors::RGBA(ALPHA_MAX_, ALPHA_MAX_, ALPHA_MAX_, (int)black_alpha_)
+        DX9::Colors::RGBA(COLOR_MAX_, COLOR_MAX_, COLOR_MAX_, black_alpha_)
     );
 
     DX9::SpriteBatch->End();
--- DirectX12 Game Solution/DirectX12 MainProject/TitleScene.h
+++ DirectX12 Game Solution/DirectX12 MainProject/TitleScene.h
@@ -41,11 +41,20 @@ public:
 private:
 	SceneBase scene_base_; /**< シーンベース */
 	DX9::SPRITE black_; /** 黒画像格納 */
-	float black_alpha_; /**< 黒画像のアルファ値 */
+	DX9::SPRITE title_text_; /**< タイトル画像格納 */
+	int text_alpha_; /**< タイトルのアルファ値　*/
+	int black_alpha_; /**< 黒画像のアルファ値 */
 	bool start_flag_; /**< ゲームを始めるか */
 
 	const int WIDTH_MAX_ = 1280; /**< 画像の横幅 */
 	const int HIGHT_MAX_ = 720; /**< 画像の縦幅 */
-	const float ALPHA_SPEED_ = 180.0f; /**< アルファ値増減速度 */
-	const float ALPHA_MAX_ = 255.0f; /**< アルファ最大値 */
+	const int COLOR_MAX_ = 255.0f; /**< 色の最大値 */
+	const int BLACK_ALPHA_SPEED_ = 180; /**< 黒画像アルファ値増減速度 */
+	const int TEXT_ALPHA_SPEED_ = 300; /**< タイトルアルファ値増減速度 */
+	const float TITLE_TEXT_POS_X_ = 640.0f; /**< タイトルX座標 */
+	const float TITLE_TEXT_POS_Y_ = 360.0f; /**< タイトルY座標 */
+	const float TITLE_TEXT_ORIGIN_X_ = 600.0f; /**< タイトルの原点X */
+	const float TITLE_TEXT_ORIGIN_Y_ = 150.0f; /**< タイトルの原点Y */
+	const int TITLE_TEXT_WIDTH_ = 1200; /**< タイトル画像の幅 */
+	const int TITLE_TEXT_HIGHT_ = 300; /**< タイトル画像の高さ */
 };
