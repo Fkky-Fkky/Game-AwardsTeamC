cc526b4dffd1d28c33918da2dbada8cf45596f4e
MSI\jfuki <jfukiage46@gmail.com>
2022-09-29 11:49:10 +0900

    [Update] 各シーン共通テキスト画像の処理関数作成

--- DirectX12 Game Solution/DirectX12 MainProject/Classes/GameScene/SceneBase.cpp
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/GameScene/SceneBase.cpp
@@ -46,7 +46,7 @@ void SceneBase::Render() const {
 }
 
 /**
-* @brief 画像読み込み
+* @brief 背景画像読み込み
 * 
 * @param[in] bg_file 画像ファイル名
 */
@@ -55,12 +55,61 @@ void SceneBase::LoadBG(const LPCWSTR bg_file) {
 }
 
 /**
-* @brief 画像描画
+* @brief 背景画像描画
 */
 void SceneBase::RenderBG() const {
     DX9::SpriteBatch->DrawSimple(
         bg_.Get(),
         SimpleMath::Vector3::Zero
+    );
+}
+
+/**
+* @brief テキストの座標設定
+* 
+* @param[in] init_pos 初期Y座標
+*/
+void SceneBase::SetTextPosY(const float init_pos) {
+    text_pos_  = SimpleMath::Vector3(TEXT_POS_X_, init_pos, 0.0f);
+}
+
+/**
+* @brief テキスト画像ファイル読み込み
+* 
+* @param[in] text_file 画像ファイル名
+*/
+void SceneBase::LoadText(const LPCWSTR text_file) {
+    text_ = DX9::Sprite::CreateFromFile(DXTK->Device9, text_file);
+}
+
+/**
+* @brief テキストの更新
+* 
+* @param[in] deltaTime 時間
+*/
+void SceneBase::UpdateText(const float deltaTime) {
+    if (text_pos_.y > MOVE_DEST_Y_) {
+        text_pos_.y = std::max(text_pos_.y - MOVE_SPEED_ * deltaTime, MOVE_DEST_Y_);
+    }
+    else {
+        text_pos_.y = std::min(text_pos_.y + MOVE_SPEED_ * deltaTime, MOVE_DEST_Y_);
+    }
+
+    text_alpha_ += ALPHA_SPEED_ * deltaTime;
+    if (text_alpha_ >= COLOR_MAX_) {
+        text_alpha_  = COLOR_MAX_;
+    }
+}
+
+/**
+* @brief テキスト画像の描画
+*/
+void SceneBase::RenderText() const {
+    DX9::SpriteBatch->DrawSimple(
+        text_.Get(),
+        text_pos_,
+        RectWH(0, 0, TEXT_WIDTH_, TEXT_HIGHT_),
+        DX9::Colors::RGBA(COLOR_MAX_, COLOR_MAX_, COLOR_MAX_, text_alpha_)
     );
 }
 
--- DirectX12 Game Solution/DirectX12 MainProject/Classes/GameScene/SceneBase.h
+++ DirectX12 Game Solution/DirectX12 MainProject/Classes/GameScene/SceneBase.h
@@ -22,6 +22,8 @@ public:
 	SceneBase() {
 		dx9GpuDescriptor = {};
 		volume_ = 0;
+		text_alpha_ = 0;
+		text_pos_ = SimpleMath::Vector3::Zero;
 	}
 
 	void LoadAssets();
@@ -29,6 +31,10 @@ public:
 
 	void LoadBG(const LPCWSTR bg_file);
 	void RenderBG()const;
+	void SetTextPosY(const float init_pos_y);
+	void LoadText(const LPCWSTR text_file);
+	void UpdateText(const float deltaTime);
+	void RenderText()const;
 	void SetBGM(const LPCWSTR bgm_file);
 	void PlayBGM()const;
 	void FadeOut(const float deltaTime);
@@ -37,9 +43,21 @@ private:
 	DX12::SPRITEBATCH    spriteBatch;
 	DX12::HGPUDESCRIPTOR dx9GpuDescriptor;
 
-	DX9::SPRITE bg_; /**< 画像格納 */
+	DX9::SPRITE bg_; /**< 背景画像格納 */
+	DX9::SPRITE text_; /**< テキスト画像格納 */
 	DX9::MEDIARENDERER bgm_; /**< BGM格納 */
+
+	int text_alpha_; /**< テキストのアルファ値 */
 	float volume_; /**< BGMの音量格納 */
+	SimpleMath::Vector3 text_pos_; /**< テキストの座標 */
+	
+	const int TEXT_WIDTH_  = 1200; /**< テキスト画像の幅 */
+	const int TEXT_HIGHT_  = 300; /**< テキスト画像の高さ */
+	const int ALPHA_SPEED_ = 300; /**< アルファ値増減速度 */
+	const int COLOR_MAX_   = 255; /**< 色の最大値 */
+	const float TEXT_POS_X_ = 40.0f; /**< テキストのX座標 */
+	const float MOVE_SPEED_  = 20.0f; /**< テキストの移動速度 */
+	const float MOVE_DEST_Y_ = 210.0f; /**< テキストの移動先 */
 	const float VOLUME_DOWN_SPEED_ = 800.0f; /**< 音量を下げる速度 */
 	const float VOLUME_MIN_ = -2000.0f; /**< 音量の最低値 */
 };
